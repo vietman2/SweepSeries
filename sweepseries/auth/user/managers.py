e56ae466a42e1915a4ccb0a810ad6d6e0f48b918
vietman2 <vietman0@gmail.com>
2024-10-22 20:29:33 +0900

    lint fix

--- sweepseries/auth/user/managers.py
+++ sweepseries/auth/user/managers.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def get_queryset(self):
-        return super().get_queryset()
-
     def create_user(self, password=None, **extra_fields):
         user = self.model(
             **extra_fields
