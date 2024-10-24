e63774122ede8f128424110443a4fb8e80d1d5f5
vietman2 <vietman0@gmail.com>
2024-10-24 15:27:53 +0900

    add login button to fallback

--- sweep/src/components/Fallbacks/Fallbacks.test.tsx
+++ sweep/src/components/Fallbacks/Fallbacks.test.tsx
@@ -1,7 +1,17 @@
+import { fireEvent } from "@testing-library/react-native";
+import { router } from "expo-router";
+
 import { LoadingComponent } from "./Loading";
 import { LoginNeeded } from "./LoginNeeded";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    canDismiss: jest.fn(),
+    dismissAll: jest.fn(),
+    replace: jest.fn(),
+  },
+}));
 jest.unmock("@components/Fallbacks");
 
 describe("<LoadingComponent>", () => {
@@ -11,7 +21,17 @@ describe("<LoadingComponent>", () => {
 });
 
 describe("<LoginNeeded>", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<LoginNeeded />);
+  it("renders correctly and handles redirect (dismissAll)", () => {
+    jest.spyOn(router, "canDismiss").mockReturnValue(true);
+    const { getByTestId } = renderWithProviders(<LoginNeeded />);
+
+    fireEvent.press(getByTestId("로그인 하러가기"));
+  });
+
+  it("renders correctly and handles redirect (nothing to dismiss)", () => {
+    jest.spyOn(router, "canDismiss").mockReturnValue(false);
+    const { getByTestId } = renderWithProviders(<LoginNeeded />);
+
+    fireEvent.press(getByTestId("로그인 하러가기"));
   });
 });
