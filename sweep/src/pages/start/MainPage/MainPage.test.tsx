b316a25b0772db9fd85f8d07634fbb113f26c0db
vietman2 <vietman0@gmail.com>
2024-10-24 16:22:50 +0900

    finish mainpage

--- sweep/src/pages/start/MainPage/MainPage.test.tsx
+++ sweep/src/pages/start/MainPage/MainPage.test.tsx
@@ -1,8 +1,21 @@
+import { fireEvent } from "@testing-library/react-native";
+
 import { MainPage } from "./MainPage";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    replace: jest.fn(),
+    push: jest.fn(),
+  },
+}));
+
 describe("<MainPage>", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<MainPage />);
+  it("renders correctly handles navigations", () => {
+    const { getByTestId, getByText } = renderWithProviders(<MainPage />);
+
+    fireEvent.press(getByText("일하러 가기"));
+    fireEvent.press(getByTestId("로그인"));
+    fireEvent.press(getByTestId("비회원으로 둘러보기"));
   });
 });
--- sweep/src/pages/start/MainPage/MainPage.tsx
+++ sweep/src/pages/start/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
 
+import { TextButton } from "@components/Buttons";
 import { AppIcon } from "@components/Icons";
 import { useTheme } from "@contexts/theme";
 import { ThemeColorType } from "@themes/colors";
@@ -9,25 +10,52 @@ export function MainPage() {
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
+  const handleSignup = () => {
+    router.push("/signup");
+  }
+
+  const handleLogin = () => {
+    router.push("/login");
+  };
+
+  const handleGuest = () => {
+    router.replace("/home");
+  };
+
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("@assets/images/splash.png")}
-        style={styles.container}
-      >
-        <Link href="/home">
-          <View style={styles.button}>
-            <AppIcon icon="calendar" color={theme.primary} size={50} />
-            <Text>일하러 가기</Text>
-          </View>
-        </Link>
-        <Link href="/login">
-          <View style={styles.button}>
-            <AppIcon icon="baseball" color={theme.primary} size={50} />
-            <Text>야구하러 가기</Text>
-          </View>
-        </Link>
-      </ImageBackground>
+      <View style={styles.wrapper}>
+        <TouchableOpacity style={styles.button} onPress={handleSignup}>
+          <AppIcon icon="calendar" color={theme.primary} size={50} />
+          <Text>일하러 가기</Text>
+        </TouchableOpacity>
+      </View>
+      <Image
+        src="https://kr.object.ncloudstorage.com/sweepdev/icons/mainlogo_large.png"
+        style={styles.image}
+      />
+      <View style={styles.wrapper}>
+        <TouchableOpacity style={styles.button} onPress={handleSignup}>
+          <AppIcon icon="baseball" color={theme.primary} size={50} />
+          <Text>야구하러 가기</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer}>
+        <TextButton
+          text="로그인"
+          onPress={handleLogin}
+          color={theme.primary}
+          backgroundColor={theme.background}
+          fontSize={18}
+        />
+        <TextButton
+          text="비회원으로 둘러보기"
+          onPress={handleGuest}
+          fontSize={18}
+          backgroundColor={theme.background}
+          color={theme.lowEmphasis}
+        />
+      </View>
     </View>
   );
 }
@@ -37,13 +65,18 @@ const createStyles = (theme: ThemeColorType) =>
     container: {
       flex: 1,
       alignItems: "center",
-      justifyContent: "space-evenly",
-      width: "100%",
-      gap: 64,
+      backgroundColor: theme.background,
+      paddingVertical: 64,
+      gap: 16,
     },
-    background: {
+    image: {
       flex: 1,
-      resizeMode: "cover",
+      width: 300,
+      resizeMode: "contain",
+    },
+    wrapper: {
+      flex: 2,
+      alignItems: "center",
       justifyContent: "center",
     },
     button: {
@@ -61,5 +94,13 @@ const createStyles = (theme: ThemeColorType) =>
       },
       shadowOpacity: 0.15,
       shadowRadius: 4,
+      elevation: 4,
+    },
+    footer: {
+      flex: 1,
+      width: "100%",
+      justifyContent: "center",
+      paddingHorizontal: 24,
+      gap: 16,
     },
   });
