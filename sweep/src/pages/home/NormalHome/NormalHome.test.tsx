78d3b4e0a9dc0042ef753b55ded0cf28403b5ea7
vietman2 <vietman0@gmail.com>
2024-10-28 22:14:48 +0900

    finish sort

--- sweep/src/pages/home/NormalHome/NormalHome.test.tsx
+++ sweep/src/pages/home/NormalHome/NormalHome.test.tsx
@@ -1,7 +1,17 @@
 import { fireEvent } from "@testing-library/react-native";
+
 import { NormalHome } from "./NormalHome";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("@gorhom/bottom-sheet", () => {
+  return {
+    __esModule: true,
+    default: "BottomSheet",
+    BottomSheetBackdrop: () => "BottomSheetBackdrop",
+    BottomSheetBackdropProps: null,
+    BottomSheetView: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 jest.mock("@fragments/Academy", () => ({
   AcademySimple: () => <div>AcademySimple</div>,
   AcademySuggest: () => <div>AcademySuggest</div>,
@@ -9,10 +19,17 @@ jest.mock("@fragments/Academy", () => ({
 }));
 
 describe("<NormalHome />", () => {
-  it("renders correctly", () => {
+  it("handles filter correctly", () => {
     const { getByTestId } = renderWithProviders(<NormalHome />);
 
     fireEvent.press(getByTestId("filter"));
     fireEvent.press(getByTestId("filter"));
+  });
+
+  it("handles sort correctly", () => {
+    const { getByTestId } = renderWithProviders(<NormalHome />);
+
+    fireEvent.press(getByTestId("sort-button"));
+    fireEvent.press(getByTestId("sort-item-인기순"));
   });
 });
--- sweep/src/pages/home/NormalHome/NormalHome.tsx
+++ sweep/src/pages/home/NormalHome/NormalHome.tsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 
 import { Filters } from "@components/Filters";
+import { AppIcon } from "@components/Icons";
 import { Scroll } from "@components/ScrollView";
 import { Searchbar } from "@components/Search";
-import { Sort } from "@components/Sorts";
 import { Text } from "@components/Texts";
 import { useTheme } from "@contexts/theme";
 import { AcademySimple, AcademySuggest, MyAcademy } from "@fragments/Academy";
@@ -19,6 +24,7 @@ export function NormalHome() {
   const [suggestions, setSuggestions] = useState<AcademySimpleType[]>([]);
   const [academies, setAcademies] = useState<AcademySimpleType[]>([]);
 
+  const ref = useRef<BottomSheet>(null);
   const [query, setQuery] = useState<string>("");
   const [selectedSort, setSelectedSort] = useState<string>("인기순");
   const [selectedFilter, setSelectedFilter] = useState<string>("");
@@ -33,57 +39,106 @@ export function NormalHome() {
     }
   };
 
+  const handleSortSelect = (sort: string) => {
+    setSelectedSort(sort);
+    ref.current?.close();
+  };
+
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...props}
+        enableTouchThrough={false}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+
   useEffect(() => {
     setSuggestions(sampleAcademies);
     setAcademies(sampleAcademies);
   }, []);
 
   return (
-    <Scroll style={styles.container}>
-      <MyAcademy />
-      <View style={styles.content}>
-        <View style={styles.header}>
-          <Text style={styles.title}>나에게 딱 맞는 캐치비 추천!</Text>
-          <Text style={styles.subtitle}>Catch B가 추천하는 아카데미/레슨</Text>
-        </View>
-        <Scroll horizontal>
-          {suggestions.map((academy) => (
-            <AcademySuggest key={academy.uuid} academy={academy} />
-          ))}
-        </Scroll>
-      </View>
-      <View style={styles.content}>
-        <View style={styles.header}>
-          <Text style={styles.title}>아카데미 찾기</Text>
-          <View style={styles.searchbar}>
-            <Searchbar
-              placeholder="제목, 내용으로 검색하세요"
-              value={query}
-              onChange={setQuery}
-              onSubmit={() => {}}
-            />
+    <>
+      <Scroll style={styles.container}>
+        <MyAcademy />
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title}>나에게 딱 맞는 캐치비 추천!</Text>
+            <Text style={styles.subtitle}>
+              Catch B가 추천하는 아카데미/레슨
+            </Text>
           </View>
-          <View style={styles.filters}>
-            <Sort
-              options={sortOptions}
-              selectedOption={selectedSort}
-              onSelect={setSelectedSort}
-            />
-            <Filters
-              filters={filters}
-              selectedFilter={selectedFilter}
-              onSelect={handleFilterSelect}
-            />
+          <Scroll horizontal>
+            {suggestions.map((academy) => (
+              <AcademySuggest key={academy.uuid} academy={academy} />
+            ))}
+          </Scroll>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title}>아카데미 찾기</Text>
+            <View style={styles.searchbar}>
+              <Searchbar
+                placeholder="제목, 내용으로 검색하세요"
+                value={query}
+                onChange={setQuery}
+                onSubmit={() => {}}
+              />
+            </View>
+            <View style={styles.filters}>
+              <TouchableOpacity
+                style={styles.sortButton}
+                onPress={() => ref.current?.expand()}
+                testID="sort-button"
+              >
+                <AppIcon icon="sort" size={18} color={theme.background} />
+                <Text style={styles.text}>{selectedSort}</Text>
+              </TouchableOpacity>
+              <Filters
+                filters={filters}
+                selectedFilter={selectedFilter}
+                onSelect={handleFilterSelect}
+              />
+            </View>
+          </View>
+          <View style={styles.academies}>
+            {academies.map((academy) => (
+              <AcademySimple key={academy.uuid} academy={academy} />
+            ))}
           </View>
         </View>
-        <View style={styles.academies}>
-          {academies.map((academy) => (
-            <AcademySimple key={academy.uuid} academy={academy} />
+        <View style={styles.void} />
+      </Scroll>
+      <BottomSheet
+        ref={ref}
+        index={-1}
+        enableDynamicSizing
+        backdropComponent={renderBackdrop}
+      >
+        <BottomSheetView style={styles.sortChoicesContainer}>
+          {sortOptions.map((option) => (
+            <TouchableOpacity
+              onPress={() => handleSortSelect(option)}
+              key={option}
+              testID={`sort-item-${option}`}
+            >
+              <Text
+                style={[
+                  styles.sortChoiceText,
+                  option === selectedSort && styles.selectedText,
+                ]}
+              >
+                {option}
+              </Text>
+            </TouchableOpacity>
           ))}
-        </View>
-      </View>
-      <View style={styles.void} />
-    </Scroll>
+        </BottomSheetView>
+      </BottomSheet>
+    </>
   );
 }
 
@@ -124,5 +179,38 @@ const createStyles = (theme: ThemeColorType) =>
     },
     void: {
       height: 24,
-    }
+    },
+    text: {
+      fontSize: 16,
+      color: theme.background,
+    },
+    sortButton: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 8,
+      paddingHorizontal: 8,
+      paddingVertical: 4,
+      gap: 4,
+      backgroundColor: theme.primary,
+      borderRadius: 4,
+      shadowColor: theme.lowEmphasis,
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.24,
+      shadowRadius: 4,
+      elevation: 4,
+    },
+    sortChoicesContainer: {
+      flex: 0,
+      minHeight: 100,
+    },
+    sortChoiceText: {
+      padding: 16,
+      textAlign: "center",
+      fontSize: 24,
+      fontWeight: "bold",
+      color: "black",
+    },
+    selectedText: {
+      color: theme.primary,
+    },
   });
