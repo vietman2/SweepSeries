2bbd71632bcb37ba220aaf09448c778640ecea05
vietman2 <vietman0@gmail.com>
2024-10-31 15:05:25 +0900

    add pro home

---
+++ sweep/src/pages/home/ProHome/ProHome.test.tsx
@@ -0,0 +1,8 @@
+import { ProHome } from "./ProHome";
+import { renderWithProviders } from "@utils/test-utils";
+
+describe("<ProHome />", () => {
+  it("renders", () => {
+    renderWithProviders(<ProHome />);
+  });
+});
--- sweep/src/pages/home/index.tsx
+++ sweep/src/pages/home/index.tsx
@@ -1,5 +1,17 @@
 import { AcademyDetail } from "./AcademyDetail/AcademyDetail";
 import { MyAcademy } from "./MyAcademy/MyAcademy";
 import { NormalHome } from "./NormalHome/NormalHome";
+import { ProHome } from "./ProHome/ProHome";
+import { useAuth } from "@contexts/auth";
 
-export { AcademyDetail, MyAcademy, NormalHome as Home };
+function Home() {
+  const { mode } = useAuth();
+
+  if (mode === "pro") {
+    return <ProHome />;
+  } else {
+    return <NormalHome />;
+  }
+}
+
+export { AcademyDetail, MyAcademy, Home };
