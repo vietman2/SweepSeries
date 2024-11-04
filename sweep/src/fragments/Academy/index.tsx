68f19dcc60b8816664abc06080ede09c676e3248
vietman2 <vietman0@gmail.com>
2024-11-04 14:03:15 +0900

    export

--- sweep/src/fragments/Academy/index.tsx
+++ sweep/src/fragments/Academy/index.tsx
@@ -1,5 +1,6 @@
 import { AcademyCard } from "./AcademyCard/AcademyCard";
+import { AcademyProfile } from "./AcademyProfile/AcademyProfile";
 import { AcademySimple } from "./AcademySimple/AcademySimple";
 import { AcademySuggest } from "./AcademySuggest/AcademySuggest";
 
-export { AcademyCard, AcademySimple, AcademySuggest };
+export { AcademyCard, AcademyProfile, AcademySimple, AcademySuggest };
