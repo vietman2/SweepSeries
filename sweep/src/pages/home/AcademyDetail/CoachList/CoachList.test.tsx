2a41ee8024973486ad4ecf6227fa7fd3b45b5b6a
vietman2 <vietman0@gmail.com>
2024-11-05 16:30:26 +0900

    add coach detail navigation

--- sweep/src/pages/home/AcademyDetail/CoachList/CoachList.test.tsx
+++ sweep/src/pages/home/AcademyDetail/CoachList/CoachList.test.tsx
@@ -1,12 +1,21 @@
+import { fireEvent } from "@testing-library/react-native";
+
 import { CoachList } from "./CoachList";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    push: jest.fn(),
+  },
+}));
 jest.mock("@fragments/Coach", () => ({
   CoachSimple: () => <></>,
 }));
 
 describe("<CoachList />", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<CoachList />);
+  it("renders correctly and handles navigation", () => {
+    const { getByTestId } = renderWithProviders(<CoachList />);
+
+    fireEvent.press(getByTestId("coach-1"));
   });
 });
