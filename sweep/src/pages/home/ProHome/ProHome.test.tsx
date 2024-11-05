fd00eb0fd8718b9a3f33c5e4fb9b60ff9dc491ae
vietman2 <vietman0@gmail.com>
2024-11-05 22:56:27 +0900

    work prohome

--- sweep/src/pages/home/ProHome/ProHome.test.tsx
+++ sweep/src/pages/home/ProHome/ProHome.test.tsx
@@ -1,8 +1,18 @@
+import { fireEvent } from "@testing-library/react-native";
+
 import { ProHome } from "./ProHome";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("@fragments/Academy", () => ({
+  AcademyCard: () => <div>AcademyCard</div>,
+  AcademySimple: () => <div>AcademySimple</div>,
+}));
+
 describe("<ProHome />", () => {
-  it("renders", () => {
-    renderWithProviders(<ProHome />);
+  it("renders and handles filters", () => {
+    const { getByTestId } = renderWithProviders(<ProHome />);
+
+    fireEvent.press(getByTestId("filter"));
+    fireEvent.press(getByTestId("filter"));
   });
 });
