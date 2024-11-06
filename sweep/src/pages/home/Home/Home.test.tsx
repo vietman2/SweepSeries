5d46ab50f573b58180decdf0ea8de782baca11a2
vietman2 <vietman0@gmail.com>
2024-11-06 12:07:10 +0900

    handle duplicates

---
+++ sweep/src/pages/home/Home/Home.test.tsx
@@ -0,0 +1,72 @@
+import { fireEvent } from "@testing-library/react-native";
+
+import { Home } from "./Home";
+import * as AuthContext from "@contexts/auth";
+import { renderWithProviders } from "@utils/test-utils";
+
+jest.mock("expo-router", () => ({
+  router: {
+    push: jest.fn(),
+  },
+}));
+jest.mock("@gorhom/bottom-sheet", () => ({
+  __esModule: true,
+  default: "BottomSheet",
+  BottomSheetBackdrop: () => "BottomSheetBackdrop",
+  BottomSheetBackdropProps: null,
+  BottomSheetView: ({ children }: { children: React.ReactNode }) => children,
+}));
+jest.mock("@contexts/auth", () => ({
+  AuthProvider: ({ children }: { children: React.ReactNode }) => (
+    <>{children}</>
+  ),
+  useAuth: jest.fn(),
+}));
+jest.mock("@fragments/Academy", () => ({
+  AcademyCard: () => "AcademyCard",
+  AcademySimple: () => "AcademySimple",
+  AcademySuggest: () => "AcademySuggest",
+}));
+
+describe("<Home />", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.spyOn(AuthContext, "useAuth").mockReturnValue({
+      login: jest.fn(),
+      logout: jest.fn(),
+      mode: "normal",
+      isAuthenticated: true,
+    });
+  });
+
+  it("handles filter correctly", () => {
+    const { getByTestId } = renderWithProviders(<Home />);
+
+    fireEvent.press(getByTestId("filter"));
+    fireEvent.press(getByTestId("filter"));
+  });
+
+  it("handles sort correctly", () => {
+    const { getByTestId } = renderWithProviders(<Home />);
+
+    fireEvent.press(getByTestId("sort-button"));
+    fireEvent.press(getByTestId("sort-item-인기순"));
+  });
+  
+  it("handles navigate to academy detail correctly", () => {
+    const { getByTestId } = renderWithProviders(<Home />);
+
+    fireEvent.press(getByTestId("academy-1"));
+  });
+
+  it("renders pro home correctly", () => {
+    jest.spyOn(AuthContext, "useAuth").mockReturnValue({
+      login: jest.fn(),
+      logout: jest.fn(),
+      mode: "pro",
+      isAuthenticated: true,
+    });
+
+    renderWithProviders(<Home />);
+  });
+});
--- sweep/src/pages/home/index.tsx
+++ sweep/src/pages/home/index.tsx
@@ -1,18 +1,6 @@
 import { AcademyDetail } from "./AcademyDetail/AcademyDetail";
 import { CoachDetail } from "./AcademyDetail/CoachDetail/CoachDetail";
+import { Home } from "./Home/Home";
 import { MyAcademy } from "./MyAcademy/MyAcademy";
-import { NormalHome } from "./NormalHome/NormalHome";
-import { ProHome } from "./ProHome/ProHome";
-import { useAuth } from "@contexts/auth";
-
-function Home() {
-  const { mode } = useAuth();
-
-  if (mode === "pro") {
-    return <ProHome />;
-  } else {
-    return <NormalHome />;
-  }
-}
 
 export { AcademyDetail, CoachDetail, MyAcademy, Home };
