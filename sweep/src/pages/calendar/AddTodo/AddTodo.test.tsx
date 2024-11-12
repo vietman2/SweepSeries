012d3b9a864ffcb742bb77530027ac0d81366591
vietman2 <vietman0@gmail.com>
2024-11-12 16:44:26 +0900

    work add todo

--- sweep/src/pages/calendar/AddTodo/AddTodo.test.tsx
+++ sweep/src/pages/calendar/AddTodo/AddTodo.test.tsx
@@ -1,8 +1,40 @@
+import { fireEvent } from "@testing-library/react-native";
+
 import { AddTodo } from "./AddTodo";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("@react-native-community/datetimepicker", () => {
+  const { TouchableOpacity } = jest.requireActual("react-native");
+
+  return {
+    __esModule: true,
+    default: ({
+      onChange,
+    }: {
+      onChange: (event: any, selectedDate?: Date) => void;
+    }) => {
+      return (
+        <>
+          <TouchableOpacity
+            onPress={() => onChange({}, new Date())}
+            testID="change-datetime"
+          />
+          <TouchableOpacity onPress={() => onChange({})} testID="cancel" />
+        </>
+      );
+    },
+    DateTimePickerEvent: jest.fn(),
+  };
+});
+jest.mock("@fragments/Schedule", () => ({
+  ScheduleInput: () => <></>,
+}));
+
 describe("<AddTodo />", () => {
   it("renders correctly", () => {
-    renderWithProviders(<AddTodo />);
+    const { getByTestId } = renderWithProviders(<AddTodo />);
+
+    fireEvent.press(getByTestId("change-datetime"));
+    fireEvent.press(getByTestId("cancel"));
   });
 });
--- sweep/src/pages/calendar/AddTodo/AddTodo.tsx
+++ sweep/src/pages/calendar/AddTodo/AddTodo.tsx
@@ -1,9 +1,75 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
-export function AddTodo() {
+import { TextInput } from "@components/Inputs";
+import { useTheme } from "@contexts/theme";
+import { ScheduleInput } from "@fragments/Schedule";
+import { ThemeColorType } from "@themes/colors";
+
+interface Props {
+  initialDate?: Date;
+}
+
+export function AddTodo({ initialDate = new Date() }: Readonly<Props>) {
+  const [todo, setTodo] = useState<string>("");
+  const [date, setDate] = useState<Date>(initialDate);
+
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  const handleDateChange = (event: any, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
   return (
-    <View>
-      <View />
+    <View style={styles.container}>
+      <View>
+        <TextInput
+          value={todo}
+          onChangeText={setTodo}
+          placeholder="할 일을 입력하세요."
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <DateTimePicker
+          mode="date"
+          value={date}
+          onChange={handleDateChange}
+          display="spinner"
+          textColor="#000"
+        />
+      </View>
+      <View>
+        <ScheduleInput
+          icon="calendar-number-2"
+          text="Calendar 1"
+          onPress={() => {}}
+        />
+        <ScheduleInput icon="clock" text="알림" onPress={() => {}} />
+        <ScheduleInput icon="palette" text="색상" onPress={() => {}} />
+        <ScheduleInput icon="repeat" text="반복" onPress={() => {}} />
+      </View>
     </View>
   );
 }
+
+const createStyles = (theme: ThemeColorType) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+      paddingHorizontal: 16,
+    },
+    wrapper: {
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 16,
+      marginHorizontal: 16,
+      paddingHorizontal: 16,
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: theme.border,
+    },
+  });
