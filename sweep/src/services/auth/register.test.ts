a4a11a03ab23e91e9593b7fc4f8dd2f840a1d14a
vietman2 <vietman0@gmail.com>
2024-11-15 05:41:49 +0900

    add check password api

--- sweep/src/services/auth/register.test.ts
+++ sweep/src/services/auth/register.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { waitFor } from "@testing-library/react-native";
 
-import { checkUsernameEmail } from "./register";
+import { checkUsernameEmail, checkPassword } from "./register";
 
 describe("checkUsernameEmail", () => {
   it("should successfully check username and email", async () => {
@@ -36,6 +36,45 @@ describe("checkUsernameEmail", () => {
 
     const response = await waitFor(() =>
       checkUsernameEmail("username", "email")
+    );
+
+    expect(response.status).toBe(400);
+  });
+});
+
+describe("checkPassword", () => {
+  it("should successfully check password", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue({
+      status: 200,
+      data: "Valid",
+    });
+
+    const response = await waitFor(() =>
+      checkPassword("password", "password")
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should handle invalid password", async () => {
+    jest.spyOn(axios, "isAxiosError").mockReturnValueOnce(true);
+    jest.spyOn(axios, "post").mockResolvedValue({
+      status: 400,
+      data: "Invalid password",
+    });
+
+    const response = await waitFor(() =>
+      checkPassword("password", "password")
+    );
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should handle server error", async () => {
+    jest.spyOn(axios, "post").mockRejectedValue(null);
+
+    const response = await waitFor(() =>
+      checkPassword("password", "password")
     );
 
     expect(response.status).toBe(400);
--- sweep/src/services/auth/register.ts
+++ sweep/src/services/auth/register.ts
@@ -24,6 +24,32 @@ export async function checkUsernameEmail(username: string, email: string) {
     return {
       status: 400,
       data: "오류가 발생했습니다.",
+    };
+  }
+}
+
+export async function checkPassword(password: string, password2: string) {
+  try {
+    const response = await axios.post("/api/check-password/", {
+      password,
+      password2,
+    });
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      return {
+        status: err.response?.status,
+        data: err.response?.data,
+      };
     }
+
+    return {
+      status: 400,
+      data: "오류가 발생했습니다.",
+    };
   }
 }
