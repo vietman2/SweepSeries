1846c6093899861dd31de418783b70ee824dc324
vietman2 <vietman0@gmail.com>
2024-11-18 11:42:23 +0900

    add urls

--- sweepseries/sweepseries/urls.py
+++ sweepseries/sweepseries/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path#, include
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -22,7 +22,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('api/', include('core.api_urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SSV.as_view(url_name='schema'), name='swagger-ui'),
