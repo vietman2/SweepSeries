2a24303d7be13bb94bd6f3852a8b82a313c78d42
vietman2 <vietman0@gmail.com>
2024-11-18 11:23:40 +0900

    route apis and add common model

---
+++ sweepseries/core/api_urls.py
@@ -0,0 +1,10 @@
+#from dj_rest_auth.jwt_auth import get_refresh_view
+from rest_framework.routers import DefaultRouter
+#from rest_framework_simplejwt.views import TokenVerifyView
+
+## Auth
+from auth.agreements.views import AgreementViewSet
+
+router = DefaultRouter()
+
+router.register(r'agreements', AgreementViewSet, basename='agreements')
