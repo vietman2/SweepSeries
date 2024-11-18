7503bc0216539a177ab20bb55f4d6954ced6311b
vietman2 <vietman0@gmail.com>
2024-11-18 11:12:51 +0900

    finish terms page

--- sweep/src/pages/auth/Register/Terms/Terms.test.tsx
+++ sweep/src/pages/auth/Register/Terms/Terms.test.tsx
@@ -1,6 +1,8 @@
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
 
 import { Terms } from "./Terms";
+import * as AgreementsAPI from "@services/auth/agreements";
+import { sampleAgreements } from "@testdata/auth";
 import { renderWithProviders } from "@utils/test-utils";
 
 jest.mock("expo-router", () => ({
@@ -8,17 +10,43 @@ jest.mock("expo-router", () => ({
     push: jest.fn(),
   },
 }));
+jest.mock("@fragments/SignUp", () => {
+  const { TouchableOpacity } = jest.requireActual("react-native");
+
+  return {
+    SignUpForm: ({
+      children,
+      buttonOnPress,
+    }: {
+      children: React.ReactNode;
+      buttonOnPress: () => void;
+    }) => (
+      <>
+        {children}
+        <TouchableOpacity onPress={buttonOnPress} testID="button" />
+      </>
+    ),
+  };
+});
 
 describe("<Terms />", () => {
-  it("renders and handles presses correctly", () => {
+  it("renders and handles presses correctly", async () => {
+    jest
+      .spyOn(AgreementsAPI, "getAgreements")
+      .mockResolvedValue(sampleAgreements);
     const { getByTestId } = renderWithProviders(<Terms />);
 
-    fireEvent.press(getByTestId("모두 동의 합니다."));
-    fireEvent.press(getByTestId("모두 동의 합니다."));
-    fireEvent.press(getByTestId("(필수) 만 14세 이상입니다."));
-    fireEvent.press(getByTestId("(필수) Catch B 서비스 이용약관"));
-    fireEvent.press(getByTestId("(필수) Catch B 개인정보 처리방침"));
-    fireEvent.press(getByTestId("(선택) 광고성/경고 알림 수신 동의"));
-    fireEvent.press(getByTestId("다음으로"));
+    await waitFor(() => {
+      fireEvent.press(getByTestId("모두 동의 합니다."));
+      fireEvent.press(getByTestId("모두 동의 합니다."));
+      fireEvent.press(getByTestId("(필수) 약관 1"));
+      fireEvent.press(getByTestId("(필수) 약관 1-right"));
+      fireEvent.press(getByTestId("button"));
+    });
+  });
+
+  it("handles error correctly", async () => {
+    jest.spyOn(AgreementsAPI, "getAgreements").mockResolvedValue(null);
+    waitFor(() => renderWithProviders(<Terms />));
   });
 });
--- sweep/src/pages/auth/Register/Terms/Terms.tsx
+++ sweep/src/pages/auth/Register/Terms/Terms.tsx
@@ -1,123 +1,110 @@
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
 import { router } from "expo-router";
 
-import { TextButton } from "@components/Buttons";
 import { Checkbox } from "@components/Checkbox";
 import { Divider } from "@components/Dividers";
-import { Text } from "@components/Texts";
-import { useTheme } from "@contexts/theme";
-import { ThemeColorType } from "@themes/colors";
+import { SignUpForm } from "@fragments/SignUp";
+import { AgreementSimpleType } from "@models/auth";
+import { getAgreements } from "@services/auth";
+
+type CheckType = {
+  id: number;
+  checked: boolean;
+  required: boolean;
+};
 
 export function Terms() {
-  const [ageChecked, setAgeChecked] = useState<boolean>(false);
-  const [termsChecked, setTermsChecked] = useState<boolean>(false);
-  const [privacyChecked, setPrivacyChecked] = useState<boolean>(false);
-  const [notificationChecked, setNotificationChecked] =
-    useState<boolean>(false);
+  const [agreements, setAgreements] = useState<AgreementSimpleType[]>([]);
+  const [checkList, setCheckList] = useState<CheckType[]>([]);
 
-  const allChecked =
-    ageChecked && termsChecked && privacyChecked && notificationChecked;
-  const isButtonActive = ageChecked && termsChecked && privacyChecked;
+  const allChecked = checkList.every((check) => check.checked);
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   const toggleCheckAll = () => {
     if (allChecked) {
-      setAgeChecked(false);
-      setTermsChecked(false);
-      setPrivacyChecked(false);
-      setNotificationChecked(false);
+      setCheckList(checkList.map((check) => ({ ...check, checked: false })));
     } else {
-      setAgeChecked(true);
-      setTermsChecked(true);
-      setPrivacyChecked(true);
-      setNotificationChecked(true);
+      setCheckList(checkList.map((check) => ({ ...check, checked: true })));
     }
   };
+
+  const isButtonActive = checkList.every(
+    (check) => !check.required || check.checked
+  );
 
   const handleButtonPress = () => {
     router.push("/signup/2");
   };
 
-  const { theme } = useTheme();
-  const styles = createStyles(theme);
+  const isChecked = (id: number) => {
+    const check = checkList.find((check) => check.id === id);
+
+    return check ? check.checked : false;
+  };
+
+  const setCheck = (id: number) => {
+    setCheckList(
+      checkList.map((check) =>
+        check.id === id ? { ...check, checked: !check.checked } : check
+      )
+    );
+  };
+
+  useEffect(() => {
+    const fetchAgreements = async () => {
+      setLoading(true);
+
+      const response = await getAgreements();
+
+      if (response) {
+        setAgreements(response);
+      } else {
+        setError(true);
+      }
+
+      setLoading(false);
+    };
+
+    fetchAgreements();
+  }, []);
+
+  useEffect(() => {
+    setCheckList(
+      agreements.map((agreement) => ({
+        id: agreement.id,
+        checked: false,
+        required: agreement.required,
+      }))
+    );
+  }, [agreements]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.page}>
-        <View style={styles.header}>
-          <Text style={styles.title}>Catch B 약관에 동의해주세요!</Text>
-          <Text style={styles.subtitle}>
-            캐치비 이용을 위해 필수 약관 동의가 필요합니다.
-          </Text>
-        </View>
-        <Divider />
-        <Checkbox
-          text="모두 동의 합니다."
-          checked={allChecked}
-          onChange={toggleCheckAll}
-        />
-        <Divider />
-        <Checkbox
-          text="(필수) 만 14세 이상입니다."
-          checked={ageChecked}
-          onChange={() => setAgeChecked(!ageChecked)}
-          grayText
-        />
-        <Checkbox
-          text="(필수) Catch B 서비스 이용약관"
-          checked={termsChecked}
-          onChange={() => setTermsChecked(!termsChecked)}
-          rightPress={() => {}}
-          grayText
-        />
-        <Checkbox
-          text="(필수) Catch B 개인정보 처리방침"
-          checked={privacyChecked}
-          onChange={() => setPrivacyChecked(!privacyChecked)}
-          rightPress={() => {}}
-          grayText
-        />
-        <Checkbox
-          text="(선택) 광고성/경고 알림 수신 동의"
-          checked={notificationChecked}
-          onChange={() => setNotificationChecked(!notificationChecked)}
-          grayText
-        />
-      </View>
-      <TextButton
-        text="다음으로"
-        backgroundColor={theme.primary}
-        onPress={handleButtonPress}
-        fontSize={18}
-        active={isButtonActive}
+    <SignUpForm
+      title="Catch B 약관에 동의해주세요!"
+      subtitle="캐치비 이용을 위해 필수 약관 동의가 필요합니다."
+      buttonText="다음으로"
+      buttonOnPress={handleButtonPress}
+      buttonDisabled={!isButtonActive}
+      loading={loading}
+      error={error}
+    >
+      <Checkbox
+        text="모두 동의 합니다."
+        checked={allChecked}
+        onChange={toggleCheckAll}
       />
-    </View>
+      <Divider />
+      {agreements.map((agreement) => (
+        <Checkbox
+          key={agreement.id}
+          text={`(${agreement.required ? "필수" : "선택"}) ${agreement.title}`}
+          checked={isChecked(agreement.id)}
+          onChange={() => setCheck(agreement.id)}
+          rightPress={agreement.has_content ? () => {} : undefined}
+        />
+      ))}
+    </SignUpForm>
   );
 }
-
-const createStyles = (theme: ThemeColorType) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingBottom: 36,
-      paddingHorizontal: 16,
-      backgroundColor: theme.background,
-    },
-    page: {
-      flex: 1,
-      paddingVertical: 16,
-      gap: 16,
-    },
-    header: {
-      gap: 8,
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-      color: theme.highEmphasis,
-    },
-    subtitle: {
-      fontSize: 16,
-      color: theme.lowEmphasis,
-    },
-  });
