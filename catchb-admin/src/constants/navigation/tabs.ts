cdb53e03adb9f61c18966e76f25cd5dec7536454
vietman2 <vietman0@gmail.com>
2024-11-22 14:31:21 +0900

    configure community tabs

--- catchb-admin/src/constants/navigation/tabs.ts
+++ catchb-admin/src/constants/navigation/tabs.ts
@@ -2,22 +2,51 @@ export type TabType = {
   title: string;
   path: string;
   pathName: string;
+  subtabs: SubTabType[];
 };
 
-export const tabs: TabType[] = [
-  {
-    title: "홈",
-    path: "/home",
-    pathName: "home",
-  },
-  {
-    title: "회원 관리",
-    path: "/users",
-    pathName: "users",
-  },
-  {
-    title: "커뮤니티 관리",
-    path: "/community",
-    pathName: "community",
-  },
-];
+export type SubTabType = {
+  title: string;
+  path: string;
+};
+
+export const Home: TabType = {
+  title: "홈",
+  path: "/home",
+  pathName: "home",
+  subtabs: [
+    {
+      title: "대시보드",
+      path: "/home",
+    },
+    {
+      title: "할 일",
+      path: "/todos",
+    },
+  ],
+};
+
+export const User: TabType = {
+  title: "회원 관리",
+  path: "/users",
+  pathName: "users",
+  subtabs: [],
+};
+
+export const Community: TabType = {
+  title: "커뮤니티 관리",
+  path: "/community",
+  pathName: "community",
+  subtabs: [
+    {
+      title: "신고 관리",
+      path: "/community/reports",
+    },
+    {
+      title: "태그 관리",
+      path: "/community/tags",
+    },
+  ],
+};
+
+export const tabs: TabType[] = [Home, User, Community];
