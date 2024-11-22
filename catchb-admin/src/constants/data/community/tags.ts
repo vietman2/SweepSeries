26ce2e1cbd83b0172a24d35a23f8a569d4879a79
vietman2 <vietman0@gmail.com>
2024-11-22 17:15:08 +0900

    add test data

---
+++ catchb-admin/src/constants/data/community/tags.ts
@@ -0,0 +1,22 @@
+import { TagType } from "@models/community";
+
+export const sampleTags: TagType[] = [
+  {
+    id: 1,
+    forum_id: 1,
+    forum_name: "forum1",
+    name: "tag1",
+    color: "color1",
+    bgcolor: "bgcolor1",
+    icon: "icon1",
+  },
+  {
+    id: 2,
+    forum_id: 2,
+    forum_name: "forum2",
+    name: "tag2",
+    color: "color2",
+    bgcolor: "bgcolor2",
+    icon: "icon2",
+  },
+];
