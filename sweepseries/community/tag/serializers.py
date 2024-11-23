a0737edc10e8c47fa99f3b32fefe2a3533ad185f
vietman2 <vietman0@gmail.com>
2024-11-23 23:41:41 +0900

    add create, update and delete apis

--- sweepseries/community/tag/serializers.py
+++ sweepseries/community/tag/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 
+from community.enums import ForumChoices
 from .models import Tag
 
 class TagSerializer(serializers.ModelSerializer):
-    forum_id = serializers.SerializerMethodField()
+    forum_id    = serializers.SerializerMethodField(read_only=True)
+    forum_name  = serializers.CharField(source='get_forum_display')
 
     class Meta:
         model = Tag
-        fields = ['id', 'forum_id', 'name', 'icon', 'color', 'bgcolor']
+        fields = ['id', 'forum_id', 'forum_name', 'name', 'icon', 'color', 'bgcolor']
 
     def get_forum_id(self, obj):
         return obj.forum
+
+    def validate_forum_name(self, value):
+        for choice in ForumChoices.choices:
+            if value == choice[1]:
+                return choice
+
+        raise serializers.ValidationError('Invalid forum name')
+
+    def save(self, **kwargs):
+        forum = self.validated_data.pop('get_forum_display')
+
+        self.validated_data['forum'] = forum[0]
+
+        return super().save(**kwargs)
--- sweepseries/community/tag/views.py
+++ sweepseries/community/tag/views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -10,11 +11,13 @@ from .serializers import TagSerializer
 class TagViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    http_method_names = ['get']
+    http_method_names = ['get', 'post', 'put', 'delete']
 
-    @extend_schema(exclude=True)
+    @extend_schema(summary='태그 조회', tags=['태그'])
     def retrieve(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(summary='태그 목록 조회', tags=['태그'])
     def list(self, request, *args, **kwargs):
@@ -27,3 +30,39 @@ class TagViewSet(ModelViewSet):
             ).data
 
         return Response(forum_tags, status=status.HTTP_200_OK)
+
+    @extend_schema(summary='태그 생성', tags=['태그'])
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        read_serializer = TagSerializer(serializer.instance)
+
+        return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+    @extend_schema(summary='태그 수정', tags=['태그'])
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=False)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(summary='태그 삭제', tags=['태그'])
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
