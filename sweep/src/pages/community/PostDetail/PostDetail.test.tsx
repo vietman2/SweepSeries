6d2262b9f9f5ca2696e84f24f45cd66b036bf9ec
vietman2 <vietman0@gmail.com>
2024-11-26 01:49:11 +0900

    apply actual api instead of mock data

--- sweep/src/pages/community/PostDetail/PostDetail.test.tsx
+++ sweep/src/pages/community/PostDetail/PostDetail.test.tsx
@@ -1,8 +1,18 @@
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
+import * as Router from "expo-router";
 
 import { PostDetail } from "./PostDetail";
+import * as AuthContext from "@contexts/auth";
+import * as PostsAPI from "@services/community/posts";
+import { samplePostDetail } from "@testdata/community";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("@contexts/auth", () => ({
+  AuthProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+  useAuth: jest.fn(),
+}));
 jest.mock("@fragments/Post", () => {
   const { TouchableOpacity } = jest.requireActual("react-native");
   return {
@@ -14,16 +24,63 @@ jest.mock("@fragments/Post", () => {
 });
 
 describe("<PostDetail />", () => {
-  it("renders and handles refresh", () => {
-    const { getByTestId } = renderWithProviders(<PostDetail />);
-
-    fireEvent.press(getByTestId("refresh"));
+  beforeEach(() => {
+    jest
+      .spyOn(Router, "useLocalSearchParams")
+      .mockReturnValue({ date: "2021-07-02" });
+    jest.spyOn(AuthContext, "useAuth").mockReturnValue({
+      isAuthenticated: true,
+      login: jest.fn(),
+      logout: jest.fn(),
+      mode: "pro",
+    });
+    jest.spyOn(PostsAPI, "getPostDetail").mockResolvedValue(samplePostDetail);
   });
 
-  it("handles recomment mode", () => {
+  it("handles bad response", async () => {
+    jest.spyOn(PostsAPI, "getPostDetail").mockResolvedValue(null);
+
     const { getByTestId } = renderWithProviders(<PostDetail />);
 
-    fireEvent.press(getByTestId("recomment"));
-    fireEvent.press(getByTestId("cancel"));
+    await waitFor(() => fireEvent.press(getByTestId("error")));
+  });
+
+  it("renders and handles refresh", async () => {
+    const { getByTestId } = renderWithProviders(<PostDetail />);
+
+    await waitFor(() => fireEvent.press(getByTestId("refresh")));
+  });
+
+  it("handles post with no comment", async () => {
+    jest
+      .spyOn(PostsAPI, "getPostDetail")
+      .mockResolvedValue({ ...samplePostDetail, comments: [] });
+    const { getByTestId } = renderWithProviders(<PostDetail />);
+
+    await waitFor(() => fireEvent.press(getByTestId("refresh")));
+  });
+
+  it("handles recomment mode", async () => {
+    const { getByTestId } = renderWithProviders(<PostDetail />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("recomment"));
+      fireEvent.press(getByTestId("cancel"));
+    });
+  });
+
+  it("handles guest mode", async () => {
+    jest.spyOn(AuthContext, "useAuth").mockReturnValue({
+      isAuthenticated: false,
+      login: jest.fn(),
+      logout: jest.fn(),
+      mode: "guest",
+    });
+    const { getByTestId } = renderWithProviders(<PostDetail />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("recomment"));
+      fireEvent.press(getByTestId("cancel"));
+    });
   });
 });
