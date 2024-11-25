419a15b5a05122be7688c783f70ada2555aac5fb
vietman2 <vietman0@gmail.com>
2024-11-25 19:40:59 +0900

    only leave user model in admin page

--- sweepseries/core/admin.py
+++ sweepseries/core/admin.py
@@ -6,20 +6,10 @@ from rest_framework.authtoken.models import TokenProxy
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
 ## Models to register
-## Auth
-from auth.agreements.models import Agreement
-from auth.person.models import Person
 from auth.user.forms import UserAdmin
 from auth.user.models import User
 
-## Community
-from community.tag.models import Tag
-
-admin.site.register(Agreement)
-admin.site.register(Person)
 admin.site.register(User, UserAdmin)
-
-admin.site.register(Tag)
 
 unnecessary_models = [Group, EmailAddress, TokenProxy, OutstandingToken, BlacklistedToken]
 
