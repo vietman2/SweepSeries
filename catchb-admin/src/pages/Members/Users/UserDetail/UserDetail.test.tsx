fa1bca6fe8fb31c3a2cfb4e20462a2e694d7a786
vietman2 <vietman0@gmail.com>
2024-11-25 16:44:19 +0900

    finish user detail page

--- catchb-admin/src/pages/Members/Users/UserDetail/UserDetail.test.tsx
+++ catchb-admin/src/pages/Members/Users/UserDetail/UserDetail.test.tsx
@@ -1,8 +1,32 @@
+import { fireEvent, screen, waitFor } from "@testing-library/react";
+import * as Router from "react-router-dom";
+
 import { UserDetail } from "./UserDetail";
+import * as MembersAPI from "@services/auth/members";
 import { renderWithProviders } from "@utils/test-utils";
+import { sampleUsers } from "@data/members";
 
 describe("<UserDetail />", () => {
-  it("renders", () => {
-    renderWithProviders(<UserDetail />);
+  beforeEach(() => {
+    jest.spyOn(Router, "useParams").mockReturnValue({ tagId: "1" });
+  });
+
+  it("should handle API error", async () => {
+    jest.spyOn(MembersAPI, "getUserDetails").mockResolvedValue(null);
+    waitFor(() => renderWithProviders(<UserDetail />));
+
+    await waitFor(() =>
+      expect(screen.getByText("새로고침")).toBeInTheDocument()
+    );
+    waitFor(() => fireEvent.click(screen.getByText("새로고침")));
+  });
+
+  it("should render user details", async () => {
+    jest.spyOn(MembersAPI, "getUserDetails").mockResolvedValue(sampleUsers[0]);
+    waitFor(() => renderWithProviders(<UserDetail />));
+
+    await waitFor(() =>
+      expect(screen.getByText("사용자 정보")).toBeInTheDocument()
+    );
   });
 });
