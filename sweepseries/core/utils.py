1072905e5c8a371e63c3c3ff34f2b2273a5a2790
vietman2 <vietman0@gmail.com>
2024-11-25 23:30:17 +0900

    add url signing method

---
+++ sweepseries/core/utils.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+import boto3
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+    region_name='kr-standard',
+    config=boto3.session.Config(signature_version='s3v4'),
+)
+
+def get_presigned_url(filename):
+    return s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+            'Key': f'{filename}',
+        },
+        ExpiresIn=300,
+    )
