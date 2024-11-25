b83a8ef222a684de4ff292790809b59ab76fd83e
vietman2 <vietman0@gmail.com>
2024-11-25 23:40:20 +0900

    fix pylint message

--- sweepseries/community/post/serializers.py
+++ sweepseries/community/post/serializers.py
@@ -40,7 +40,8 @@ class PostSimpleSerializer(serializers.ModelSerializer):
     def get_num_likes(self, obj):
         return obj.post_likes.count()
 
-    def get_num_comments(self, obj):    ## pylint: disable=unused-argument // TODO: Remove this
+    def get_num_comments(self, obj):    ## pylint: disable=unused-argument
+        ## TODO: remove pylint message
         return 0
         #return obj.comments.filter(is_deleted=False).count()
 
