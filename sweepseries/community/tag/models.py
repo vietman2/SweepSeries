6f4e881cd200e878a07032a93f21596aefa41a43
vietman2 <vietman0@gmail.com>
2024-11-25 19:40:05 +0900

    apply

--- sweepseries/community/tag/models.py
+++ sweepseries/community/tag/models.py
@@ -12,11 +12,6 @@ class Tag(models.Model):
 
     objects = models.Manager()
 
-    def __str__(self):
-        return f"{self.name}"
-
     class Meta:
         db_table = 'tag'
-        verbose_name = '게시글 태그'
-        verbose_name_plural = '게시글 태그'
         ordering = ['id']
