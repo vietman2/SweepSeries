d6e3c59a66b75a2b6275022df42f38f5a0f7d889
vietman2 <vietman0@gmail.com>
2024-11-25 22:35:30 +0900

    refactor and modify post models

--- sweep/src/constants/models/community/post.ts
+++ sweep/src/constants/models/community/post.ts
@@ -1,3 +1,6 @@
+import { AuthorType } from "./author";
+import { CommentType } from "./comment";
+
 export type TagType = {
   id: number;
   forum_id: number;
@@ -9,17 +12,16 @@ export type TagType = {
 
 export type PostSimpleType = {
   id: number;
-  author_uuid: string;
-  author_nickname: string;
+  tag: TagType;
   title: string;
   content: string;
-  tag: TagType;
   image: string | null;
-  num_clicks: number;
+  created_at: string;
+  author: AuthorType;
+  num_views: number;
   num_comments: number;
   num_likes: number;
   is_liked: boolean;
-  created_at: string;
 };
 
 type ImageType = {
@@ -48,31 +50,4 @@ export type PostDetailType = {
 
   created_at: string;
   updated_at: string;
-};
-
-export type CommentType = {
-  id: number;
-  commenter_uuid: string;
-  commenter_nickname: string;
-  content: string;
-  created_at: string;
-  num_likes: number;
-  num_recomments: number;
-
-  recomments: ReCommentType[];
-
-  is_liked: boolean;
-  is_my_comment: boolean;
-};
-
-export type ReCommentType = {
-  id: number;
-  commenter_uuid: string;
-  commenter_nickname: string;
-  content: string;
-  created_at: string;
-  num_likes: number;
-
-  is_liked: boolean;
-  is_my_recomment: boolean;
 };
---
+++ sweep/src/constants/testdata/community/authors.ts
@@ -0,0 +1,8 @@
+import { AuthorType } from "@models/community";
+
+export const sampleAuthor: AuthorType = {
+  profile_image: "https://via.placeholder.com/150",
+  color: "#000000",
+  nickname: "야구쟁이",
+  introduction: "야구를 좋아하는 사람입니다.",
+};
