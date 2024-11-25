f6cb0f03ba8883ecbfe24da6b63ea6fabe6a4f40
vietman2 <vietman0@gmail.com>
2024-11-25 23:10:08 +0900

    change market display name

--- sweepseries/community/enums.py
+++ sweepseries/community/enums.py
@@ -3,7 +3,7 @@ from django.db import models
 class ForumChoices(models.IntegerChoices):
     DUGOUT  = 1, "덕아웃"
     RECRUIT = 2, "드래프트"
-    MARKET  = 3, "장터"
+    MARKET  = 3, "마켓"
     STEAL   = 4, "스틸"
 
 class ReportReason(models.TextChoices):
