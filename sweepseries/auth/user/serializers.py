8fde65f7807abead12d2fcb8de20eda263c78247
vietman2 <vietman0@gmail.com>
2024-11-27 01:08:38 +0900

    add return serializer

--- sweepseries/auth/user/serializers.py
+++ sweepseries/auth/user/serializers.py
@@ -4,6 +4,18 @@ from auth.person.serializers import PersonSerializer
 from auth.userprofile.serializers import UserProfileSerializer
 from .models import User
 
+class UserAuthSerializer(serializers.ModelSerializer):
+    uuid        = serializers.UUIDField(read_only=True)
+    username    = serializers.CharField(read_only=True)
+    profiles    = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['uuid', 'username', 'profiles']
+
+    def get_profiles(self, obj):
+        return obj.profiles.values_list('id', flat=True)
+
 class UserSerializer(serializers.ModelSerializer):
     uuid        = serializers.UUIDField(read_only=True)
     username    = serializers.CharField(read_only=True)
