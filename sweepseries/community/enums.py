c14d03fb2bf342ba6564617c8e3bc2a7dc6c8875
vietman2 <vietman0@gmail.com>
2024-11-28 01:16:14 +0900

    simplify review status

--- sweepseries/community/enums.py
+++ sweepseries/community/enums.py
@@ -17,13 +17,7 @@ class ReportReason(models.TextChoices):
     WRONG_CATEGORY  = 'WC', '잘못된 게시판/태그'
     OTHER           = 'OT', '기타'
 
-class ReviewStatus(models.IntegerChoices):
+class ReportStatus(models.IntegerChoices):
     SUBMITTED       = 0, '제출됨'
     UNDER_REVIEW    = 1, '검토중'
     REVIEWED        = 2, '검토 완료'
-
-class ReportStatus(models.IntegerChoices):
-    PENDING         = 0, '대기중'
-    ACCEPTED        = 1, '수락됨'
-    REJECTED        = 2, '거부됨'
-    PARTIAL         = 3, '일부 수락됨'
