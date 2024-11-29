87ccdd950a5cd1fcda312b4199d90fc875056c53
vietman2 <vietman0@gmail.com>
2024-11-29 22:59:23 +0900

    integrate apis

--- catchb-admin/src/services/community/index.ts
+++ catchb-admin/src/services/community/index.ts
@@ -1,10 +1,26 @@
-import { getPostReports, getPostReportDetails, updatePostReport } from "./reports";
+import {
+  getPostReports,
+  getPostReportDetails,
+  updatePostReport,
+  getCommentReports,
+  getCommentReportDetails,
+  updateCommentReport,
+  getReCommentReports,
+  getReCommentReportDetails,
+  updateReCommentReport,
+} from "./reports";
 import { getTag, getTags, createTag, deleteTag, updateTag } from "./tags";
 
 export {
   getPostReports,
   getPostReportDetails,
   updatePostReport,
+  getCommentReports,
+  getCommentReportDetails,
+  updateCommentReport,
+  getReCommentReports,
+  getReCommentReportDetails,
+  updateReCommentReport,
   getTag,
   getTags,
   createTag,
--- catchb-admin/src/services/community/reports.test.ts
+++ catchb-admin/src/services/community/reports.test.ts
@@ -4,6 +4,12 @@ import {
   getPostReports,
   getPostReportDetails,
   updatePostReport,
+  getCommentReports,
+  getCommentReportDetails,
+  updateCommentReport,
+  getReCommentReports,
+  getReCommentReportDetails,
+  updateReCommentReport,
 } from "./reports";
 
 describe("getPostReports", () => {
@@ -67,6 +73,138 @@ describe("updatePostReport", () => {
     jest.spyOn(axios, "patch").mockRejectedValue(new Error());
 
     const response = await updatePostReport("1", true, "feedback");
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("getCommentReports", () => {
+  it("should return an array of comment reports if the request is successful", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: [] });
+
+    const response = await getCommentReports();
+
+    expect(response).toEqual([]);
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error());
+
+    const response = await getCommentReports();
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("getCommentReportDetails", () => {
+  it("should return a comment report object if the request is successful", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: {} });
+
+    const response = await getCommentReportDetails("1");
+
+    expect(response).toEqual({});
+  });
+
+  it("should return null if reportId is undefined", async () => {
+    const response = await getCommentReportDetails(undefined);
+
+    expect(response).toBeNull();
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error());
+
+    const response = await getCommentReportDetails("1");
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("updateCommentReport", () => {
+  it("should return true if the request is successful", async () => {
+    jest.spyOn(axios, "patch").mockResolvedValue({ data: {} });
+
+    const response = await updateCommentReport("1", true, "feedback");
+
+    expect(response).toBeTruthy();
+  });
+
+  it("should return null if reportId is undefined", async () => {
+    const response = await updateCommentReport(undefined, true, "feedback");
+
+    expect(response).toBeNull();
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+
+    const response = await updateCommentReport("1", true, "feedback");
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("getReCommentReports", () => {
+  it("should return an array of recomment reports if the request is successful", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: [] });
+
+    const response = await getReCommentReports();
+
+    expect(response).toEqual([]);
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error());
+
+    const response = await getReCommentReports();
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("getReCommentReportDetails", () => {
+  it("should return a recomment report object if the request is successful", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: {} });
+
+    const response = await getReCommentReportDetails("1");
+
+    expect(response).toEqual({});
+  });
+
+  it("should return null if reportId is undefined", async () => {
+    const response = await getReCommentReportDetails(undefined);
+
+    expect(response).toBeNull();
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error());
+
+    const response = await getReCommentReportDetails("1");
+
+    expect(response).toBeNull();
+  });
+});
+
+describe("updateReCommentReport", () => {
+  it("should return true if the request is successful", async () => {
+    jest.spyOn(axios, "patch").mockResolvedValue({ data: {} });
+
+    const response = await updateReCommentReport("1", true, "feedback");
+
+    expect(response).toBeTruthy();
+  });
+
+  it("should return null if reportId is undefined", async () => {
+    const response = await updateReCommentReport(undefined, true, "feedback");
+
+    expect(response).toBeNull();
+  });
+
+  it("should return null if the request is unsuccessful", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+
+    const response = await updateReCommentReport("1", true, "feedback");
 
     expect(response).toBeNull();
   });
