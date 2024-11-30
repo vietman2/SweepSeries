dea33a13f7734389f30449d642572ce2f6122852
vietman2 <vietman0@gmail.com>
2024-11-30 12:31:50 +0900

    add content input:

--- catchb-admin/src/components/Inputs/Inputs.test.tsx
+++ catchb-admin/src/components/Inputs/Inputs.test.tsx
@@ -1,9 +1,31 @@
 import { fireEvent, screen } from "@testing-library/react";
 
+import { ContentInput } from "./ContentInput";
 import { TextInput } from "./TextInput";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("react-quill", () => {
+  const { forwardRef } = jest.requireActual("react");
+
+  const Component = forwardRef(({}, ref: React.RefObject<HTMLDivElement>) => {
+    return <div ref={ref} data-testid="quill" />;
+  });
+
+  return {
+    __esModule: true,
+    default: Component,
+  };
+});
+jest.mock("react-quill/dist/quill.snow.css", () => {
+  return {};
+});
 jest.unmock("@components/Inputs");
+
+describe("<ContentInput />", () => {
+  it("renders a content input element", () => {
+    renderWithProviders(<ContentInput content="" setContent={jest.fn()} />);
+  });
+});
 
 describe("<TextInput />", () => {
   it("renders an input element", () => {
