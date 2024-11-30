f4170453607c7839d6fa1b5976006a647e6d3034
vietman2 <vietman0@gmail.com>
2024-11-30 17:26:03 +0900

    update model

--- sweepseries/auth/agreements/models.py
+++ sweepseries/auth/agreements/models.py
@@ -3,20 +3,37 @@ from django.db import models
 from core.models import TimeStampedModel
 from auth.user.models import User
 
-class Agreement(TimeStampedModel):
+class Agreement(models.Model):
     title       = models.CharField(max_length=100)
-    content     = models.TextField()
     required    = models.BooleanField(default=False)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    deleted     = models.BooleanField(default=False)
+    deleted_at  = models.DateTimeField(null=True, blank=True)
 
     objects = models.Manager()
 
     class Meta:
         db_table = 'agreements'
+        ordering = ['deleted', '-required']
+
+class AgreementVersion(models.Model):
+    agreement   = models.ForeignKey(Agreement, on_delete=models.CASCADE, related_name='versions')
+    content     = models.TextField()
+    summary     = models.TextField()
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
+
+    class Meta:
+        db_table = 'agreement_versions'
+        unique_together = ('agreement', 'created_at')
+        ordering = ['-created_at']
 
 class UserAgreement(TimeStampedModel):
     user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name='agreements')
     agreement   = models.ForeignKey(Agreement, on_delete=models.CASCADE, related_name='users')
     agreed      = models.BooleanField(default=False)
+    agreed_at   = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
 
