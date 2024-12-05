e162fb952532e6887d4c94b63acd63c93dd6f99f
vietman2 <vietman0@gmail.com>
2024-12-05 13:19:20 +0900

    add test data

---
+++ catchb-admin/src/constants/data/apps/faqs.ts
@@ -0,0 +1,16 @@
+import { FAQType } from "@models/apps";
+
+export const sampleFAQs: FAQType[] = [
+  {
+    id: 1,
+    question: "질문1",
+    answer: "답변1",
+    is_active: true,
+  },
+  {
+    id: 2,
+    question: "질문2",
+    answer: "답변2",
+    is_active: false,
+  },
+];
