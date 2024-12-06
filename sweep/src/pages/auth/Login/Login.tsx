aca813b0fb4bbeb8dabc2b93c0c4a2799f97fcda
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-06 16:25:34 +0900

    fix login

--- sweep/src/pages/auth/Login/Login.tsx
+++ sweep/src/pages/auth/Login/Login.tsx
@@ -76,7 +76,7 @@ export function Login() {
 
       const response = await naverLoginRequest(profile);
       if (response) {
-        login("normal", response.user);
+        login("normal", response.user.profile);
         router.replace("/home");
       } else {
         alert(
