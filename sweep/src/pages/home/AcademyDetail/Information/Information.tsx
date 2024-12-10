16bdbc5cc579a8439066434a5aae3ef004544912
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-11 00:47:59 +0900

    handle props

--- sweep/src/pages/home/AcademyDetail/Information/Information.tsx
+++ sweep/src/pages/home/AcademyDetail/Information/Information.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Image, StyleSheet, View } from "react-native";
 
 import { Divider } from "@components/Dividers";
@@ -7,34 +6,34 @@ import { Text } from "@components/Texts";
 import { useTheme } from "@contexts/theme";
 import { Facilities, Introduction, WorkingHours } from "@fragments/Academy";
 import { AcademyDetailType } from "@models/products";
-import { sampleAcademyDetail } from "@testdata/products";
 import { ThemeColorType } from "@themes/colors";
 
-export function Information() {
-  const [academy, setAcademy] = useState<AcademyDetailType>();
+interface Props {
+  academy: AcademyDetailType;
+}
 
+export function Information({ academy }: Readonly<Props>) {
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
-  useEffect(() => {
-    setAcademy(sampleAcademyDetail);
-  }, []);
-
-  if (!academy) return null;
-
   return (
     <View style={styles.container}>
-      <Subtitle title="아카데미 소개" />
       <Introduction introduction={academy.introduction} />
       <Divider />
-      <Subtitle title="운영시간" />
+      {/*
       <WorkingHours workingHours={academy.working_hours} />
+      <Divider />*/}
+      <Facilities
+        facilities={academy.convenience}
+        type="구비장비"
+        options={[]}
+      />
       <Divider />
-      <Subtitle title="구비시설" />
-      <Facilities facilities={academy.facilities} type="구비장비" />
-      <Divider />
-      <Subtitle title="편의시설 및 서비스" />
-      <Facilities facilities={academy.facilities} type="편의시설" />
+      <Facilities
+        facilities={academy.convenience}
+        type="편의시설"
+        options={[]}
+      />
       <Divider />
       <Subtitle title="지도" />
       <View>
