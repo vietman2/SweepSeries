f0972212ad52e2cb235ffd2cc96ccfef1822392a
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-11 00:46:33 +0900

    replace with actual fetch logic

--- sweep/src/pages/home/AcademyDetail/AcademyDetail.tsx
+++ sweep/src/pages/home/AcademyDetail/AcademyDetail.tsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react";
 import { Tabs } from "react-native-collapsible-tab-view";
+import { useLocalSearchParams } from "expo-router";
 
 import { CoachList } from "./CoachList/CoachList";
 import { Information } from "./Information/Information";
 import { NoticeList } from "./NoticeList/NoticeList";
 import { ProgramList } from "./ProgramList/ProgramList";
 import { ReviewList } from "./ReviewList/ReviewList";
+import { ErrorPage } from "@components/Fallbacks";
 import { CollapsibleTab } from "@components/Tabs";
 import { AcademyProfile } from "@fragments/Academy";
+import { AcademyDetailType } from "@models/products";
+import { getAcademyDetail } from "@services/products";
 
 export function AcademyDetail() {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const [academy, setAcademy] = useState<AcademyDetailType>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getAcademyDetail(id);
+
+      if (response) {
+        setAcademy(response);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!academy) return <ErrorPage />;
+
   return (
     <Tabs.Container
-      renderHeader={() => <AcademyProfile />}
+      renderHeader={() => <AcademyProfile academy={academy} />}
       renderTabBar={(props) => <CollapsibleTab {...props} />}
       pagerProps={{ scrollEnabled: false }}
       headerContainerStyle={{ shadowOpacity: 0 }}
@@ -19,7 +41,7 @@ export function AcademyDetail() {
     >
       <Tabs.Tab name="정보">
         <Tabs.ScrollView>
-          <Information />
+          <Information academy={academy} />
         </Tabs.ScrollView>
       </Tabs.Tab>
       <Tabs.Tab name="프로그램">
