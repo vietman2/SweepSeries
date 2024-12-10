ec5378ce17b2a0f9a1ffbb423b83870548ba5c09
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-11 00:50:20 +0900

    fetch options

--- sweep/src/pages/front/Profile/Profile.tsx
+++ sweep/src/pages/front/Profile/Profile.tsx
@@ -10,36 +10,64 @@ import {
   AcademyProfile,
   Facilities,
   Introduction,
-  WorkingHours,
+  //WorkingHours,
 } from "@fragments/Academy";
-import { AcademyDetailType } from "@models/products";
-import { sampleAcademyDetail } from "@testdata/products";
+import { AcademyDetailType, FacilityType } from "@models/products";
+import { getFacilityOptions } from "@services/products";
 import { ThemeColorType } from "@themes/colors";
 
-export function ProfileManagement() {
-  const [academy, setAcademy] = useState<AcademyDetailType>();
+interface Props {
+  academy: AcademyDetailType;
+  onRefresh?: () => void;
+}
+
+export function ProfileManagement({ academy, onRefresh }: Readonly<Props>) {
+  const [facilityOptions, setFacilityOptions] = useState<FacilityType[]>([]);
 
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
   useEffect(() => {
-    setAcademy(sampleAcademyDetail);
-  }, []);
+    const fetchOptions = async () => {
+      const options = await getFacilityOptions();
 
-  if (!academy) return null;
+      if (options) {
+        setFacilityOptions(options);
+      }
+    };
+
+    fetchOptions();
+  }, []);
 
   return (
     <Scroll style={styles.container}>
-      <AcademyProfile pro />
+      <AcademyProfile pro academy={academy} />
       <View style={styles.content}>
         <Divider />
-        <Introduction introduction={academy.introduction} />
+        <Introduction
+          introduction={academy.introduction}
+          edit
+          onRefresh={onRefresh}
+        />
         <Divider />
-        <WorkingHours workingHours={academy.working_hours} />
+        {/*
+        <WorkingHours workingHours={academy.working_hours} edit />
+        <Divider />*/}
+        <Facilities
+          facilities={academy.convenience}
+          options={facilityOptions}
+          type="구비장비"
+          edit
+          onRefresh={onRefresh}
+        />
         <Divider />
-        <Facilities facilities={academy.facilities} type="구비장비" />
-        <Divider />
-        <Facilities facilities={academy.facilities} type="편의시설" />
+        <Facilities
+          facilities={academy.convenience}
+          options={facilityOptions}
+          type="편의시설"
+          edit
+          onRefresh={onRefresh}
+        />
         <Divider />
         <Text style={styles.subtitle}>지도</Text>
         <View>
