f348abe21f9b5067d0eaf33d9e6262b27021150f
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-12 15:39:41 +0900

    add coach accept and reject logics

--- sweep/src/fragments/Coach/CoachRequest/CoachRequest.test.tsx
+++ sweep/src/fragments/Coach/CoachRequest/CoachRequest.test.tsx
@@ -1,9 +1,40 @@
+import { fireEvent, waitFor } from "@testing-library/react-native";
+
 import { CoachRequest } from "./CoachRequest";
-import { sampleCoachRequests } from "@testdata/products";
+import * as CoachesAPI from "@services/products/coach";
+import { sampleCoaches } from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<CoachRequest />", () => {
-  it("should render without crashing", () => {
-    renderWithProviders(<CoachRequest coach={sampleCoachRequests[0]} />);
+  it("handles accept", async () => {
+    jest.spyOn(CoachesAPI, "acceptCoach").mockResolvedValueOnce(true);
+    jest.spyOn(CoachesAPI, "acceptCoach").mockResolvedValueOnce(null);
+    const { getByTestId } = renderWithProviders(
+      <CoachRequest coach={sampleCoaches[0]} onRefresh={jest.fn()} />
+    );
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("accept"));
+    });
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("accept"));
+    });
+  });
+
+  it("handles reject", async () => {
+    jest.spyOn(CoachesAPI, "rejectCoach").mockResolvedValueOnce(true);
+    jest.spyOn(CoachesAPI, "rejectCoach").mockResolvedValueOnce(null);
+    const { getByTestId } = renderWithProviders(
+      <CoachRequest coach={sampleCoaches[0]} onRefresh={jest.fn()} />
+    );
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("reject"));
+    });
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("reject"));
+    });
   });
 });
