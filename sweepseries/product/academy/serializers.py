1f04ecf4191efb5406763564c362fce449ea79b4
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-12 21:44:08 +0900

    minor

--- sweepseries/product/academy/serializers.py
+++ sweepseries/product/academy/serializers.py
@@ -103,6 +103,7 @@ class AcademyDetailSerializer(serializers.ModelSerializer):
         return get_presigned_url(obj.address.map_image)
 
     def get_images(self, obj):
+        print(obj)
         return []
 
     def get_rating(self, obj):
