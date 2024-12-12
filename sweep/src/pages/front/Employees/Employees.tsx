88433050255aee0b1be0065c5c1062150ca15959
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-12 15:16:25 +0900

    integrate actual data

--- sweep/src/pages/front/Employees/Employees.tsx
+++ sweep/src/pages/front/Employees/Employees.tsx
@@ -1,42 +1,73 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 
 import { Scroll } from "@components/ScrollView";
 import { Text } from "@components/Texts";
 import { useTheme } from "@contexts/theme";
 import { CoachRequest, CoachSimple } from "@fragments/Coach";
-import { CoachRequestType, CoachSimpleType } from "@models/products";
-import { sampleCoaches, sampleCoachRequests } from "@testdata/products";
+import { CoachSimpleType } from "@models/products";
+import { getEmployedCoaches } from "@services/products";
 import { ThemeColorType } from "@themes/colors";
 
 export function EmployeeManagement() {
+  const { id } = useLocalSearchParams<{ id: string }>();
   const [coaches, setCoaches] = useState<CoachSimpleType[]>([]);
-  const [requests, setRequests] = useState<CoachRequestType[]>([]);
+  const [requests, setRequests] = useState<CoachSimpleType[]>([]);
+
+  const [refreshCount, setRefreshCount] = useState(0);
 
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
+  const handleRefresh = () => {
+    setRefreshCount((prev) => prev + 1);
+  };
+
   useEffect(() => {
-    setCoaches(sampleCoaches);
-    setRequests(sampleCoachRequests);
-  }, []);
+    const fetchData = async () => {
+      const response = await getEmployedCoaches(id);
+
+      if (response) {
+        setCoaches(response.accepted);
+        setRequests(response.pending);
+      } else {
+        setCoaches([]);
+        setRequests([]);
+      }
+    };
+
+    fetchData();
+  }, [refreshCount]);
 
   return (
     <Scroll style={styles.container}>
       <View style={styles.wrapper}>
-        <View style={styles.content}>
-          <Text style={styles.title}>코치등록 요청</Text>
-          <Scroll horizontal>
-            {requests.map((request) => (
-              <CoachRequest key={request.uuid} coach={request} />
-            ))}
-          </Scroll>
-        </View>
+        {requests.length > 0 && (
+          <View style={styles.content}>
+            <Text style={styles.title}>코치등록 요청</Text>
+            <Scroll horizontal>
+              {requests.map((request) => (
+                <CoachRequest
+                  key={request.uuid}
+                  coach={request}
+                  onRefresh={handleRefresh}
+                />
+              ))}
+            </Scroll>
+          </View>
+        )}
         <View style={styles.content}>
           <Text style={styles.title}>코치 관리</Text>
-          {coaches.map((coach) => (
-            <CoachSimple key={coach.uuid} coach={coach} />
-          ))}
+          {coaches.length > 0 ? (
+            <>
+              {coaches.map((coach) => (
+                <CoachSimple key={coach.uuid} coach={coach} />
+              ))}
+            </>
+          ) : (
+            <Text>등록된 코치가 없습니다.</Text>
+          )}
         </View>
       </View>
     </Scroll>
