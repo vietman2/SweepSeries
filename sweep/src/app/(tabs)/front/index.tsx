6103432fe8da443461359f8cced981fc48366066
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-13 17:36:09 +0900

    reconfigure navigation

--- sweep/src/app/(tabs)/front/index.tsx
+++ sweep/src/app/(tabs)/front/index.tsx
@@ -1,27 +1,3 @@
-import { useEffect, useState } from "react";
-import { View } from "react-native";
-import { Redirect } from "expo-router";
+import { Front } from "@pages/front";
 
-import { getMyAcademies } from "@services/products";
-
-export default function Front() {
-  const [academyId, setAcademyId] = useState<string>("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getMyAcademies();
-
-      if (response) {
-        setAcademyId(response.academies[0]);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (academyId) {
-    return <Redirect href={`/front/academy/${academyId}`} />;
-  }
-
-  return <View />;
-}
+export default Front;
