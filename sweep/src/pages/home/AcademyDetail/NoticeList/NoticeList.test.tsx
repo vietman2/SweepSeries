c7cc43d8b5c463f82d9443d0bdb304eaf0318844
vietman2 <vietman0@gmail.com>
2024-12-16 11:02:32 +0900

    add api

--- sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.test.tsx
+++ sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.test.tsx
@@ -1,12 +1,25 @@
 import { NoticeList } from "./NoticeList";
+import * as NoticesAPI from "@services/products/notices";
+import { sampleNotices } from "@testdata/products";
+import { waitFor } from "@testing-library/react-native";
 import { renderWithProviders } from "@utils/test-utils";
 
 jest.mock("@fragments/Notice", () => ({
-  NoticeSimple: () => <></>,
+  NoticeSimple: () => "NoticeSimple",
 }));
 
 describe("<NoticeList />", () => {
-  it("renders correctly", () => {
+  it("renders correctly", async () => {
+    jest.spyOn(NoticesAPI, "getNotices").mockResolvedValueOnce(sampleNotices);
     renderWithProviders(<NoticeList />);
+
+    await waitFor(() => expect("NoticeSimple").toBeTruthy());
+  });
+  
+  it("handles api error", async () => {
+    jest.spyOn(NoticesAPI, "getNotices").mockResolvedValueOnce(null);
+    renderWithProviders(<NoticeList />);
+
+    await waitFor(() => expect("소식이 없습니다.").toBeTruthy());
   });
 });
--- sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.tsx
+++ sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.tsx
@@ -1,31 +1,52 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 
 import { Divider } from "@components/Dividers";
+import { Text } from "@components/Texts";
 import { useTheme } from "@contexts/theme";
 import { NoticeSimple } from "@fragments/Notice";
 import { NoticeSimpleType } from "@models/products";
-import { sampleNotices } from "@testdata/products";
+import { getNotices } from "@services/products";
 import { ThemeColorType } from "@themes/colors";
 
 export function NoticeList() {
   const [notices, setNotices] = useState<NoticeSimpleType[]>([]);
+  const { id } = useLocalSearchParams<{ id: string }>();
 
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
   useEffect(() => {
-    setNotices(sampleNotices);
+    const fetchData = async () => {
+      const response = await getNotices(id);
+
+      if (response) {
+        setNotices(response);
+      } else {
+        setNotices([]);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <View style={styles.container}>
-      {notices.map((notice) => (
-        <View key={notice.id} style={styles.notice}>
-          <NoticeSimple key={notice.id} notice={notice} />
-          <Divider />
+      {notices.length > 0 ? (
+        <>
+          {notices.map((notice) => (
+            <View key={notice.id} style={styles.notice}>
+              <NoticeSimple key={notice.id} notice={notice} />
+              <Divider />
+            </View>
+          ))}
+        </>
+      ) : (
+        <View style={styles.emptyContainer}>
+          <Text style={styles.emptyText}>소식이 없습니다.</Text>
         </View>
-      ))}
+      )}
     </View>
   );
 }
@@ -41,4 +62,13 @@ const createStyles = (theme: ThemeColorType) =>
     notice: {
       gap: 16,
     },
+    emptyContainer: {
+      flex: 0.5,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    emptyText: {
+      fontSize: 24,
+      color: theme.mediumEmphasis,
+    }
   });
