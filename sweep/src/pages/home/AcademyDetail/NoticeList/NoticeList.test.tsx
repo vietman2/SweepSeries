eda24025000d7cb835c81bb68bd411609fa64963
vietman2 <vietman0@gmail.com>
2024-12-16 11:18:47 +0900

    import order

--- sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.test.tsx
+++ sweep/src/pages/home/AcademyDetail/NoticeList/NoticeList.test.tsx
@@ -1,7 +1,8 @@
+import { waitFor } from "@testing-library/react-native";
+
 import { NoticeList } from "./NoticeList";
 import * as NoticesAPI from "@services/products/notices";
 import { sampleNotices } from "@testdata/products";
-import { waitFor } from "@testing-library/react-native";
 import { renderWithProviders } from "@utils/test-utils";
 
 jest.mock("@fragments/Notice", () => ({
