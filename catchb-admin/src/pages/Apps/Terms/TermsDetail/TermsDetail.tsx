3ed2135c10f34baa11f9e01e80c1e35c033c2107
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-01-07 23:35:00 +0900

    minor lint fix

--- catchb-admin/src/pages/Apps/Terms/TermsDetail/TermsDetail.tsx
+++ catchb-admin/src/pages/Apps/Terms/TermsDetail/TermsDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
