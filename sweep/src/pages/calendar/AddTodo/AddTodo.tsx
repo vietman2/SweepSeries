3ed6fb693b9ca7eca17edc98b074c77622d8f572
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-01-17 17:02:09 +0900

    finish addtodo page

--- sweep/src/pages/calendar/AddTodo/AddTodo.tsx
+++ sweep/src/pages/calendar/AddTodo/AddTodo.tsx
@@ -1,68 +1,127 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
 import DateTimePicker, {
   DateTimePickerEvent,
 } from "@react-native-community/datetimepicker";
 
 import { TextButton } from "@components/Buttons";
+import { LoadingComponent } from "@components/Fallbacks";
 import { TextInput } from "@components/Inputs";
-import { useTheme } from "@contexts/theme";
-import { ScheduleInput } from "@fragments/Schedule";
-import { ThemeColorType } from "@themes/colors";
 import { Scroll } from "@components/ScrollView";
+import { Text } from "@components/Texts";
+import { useCalendar } from "@contexts/calendar";
+import { useTheme } from "@contexts/theme";
+import { ColorModal } from "@fragments/Calendar";
+import { ScheduleInput } from "@fragments/Schedule";
+import { alert } from "@services/alert";
+import { createTodo } from "@services/calendar";
+import { ThemeColorType } from "@themes/colors";
+import { formatDate } from "@utils/formatters";
 
-interface Props {
-  initialDate?: Date;
-}
-
-export function AddTodo({ initialDate = new Date() }: Readonly<Props>) {
+export function AddTodo() {
   const [todo, setTodo] = useState<string>("");
-  const [date, setDate] = useState<Date>(initialDate);
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [color, setColor] = useState<string>("#FF6B6B");
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
+  const { date } = useLocalSearchParams<{ date: string }>();
+  const { selectedCalendar } = useCalendar();
   const { theme } = useTheme();
   const styles = createStyles(theme);
+
+  const goBack = () => {
+    router.back();
+  };
+
+  const openColorModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeColorModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleUpdateColor = (color: string) => {
+    setColor(color);
+    closeColorModal();
+  };
 
   const handleDateChange = (
     event: DateTimePickerEvent,
     selectedDate?: Date
   ) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
+    const currentDate = selectedDate || new Date(date);
+    setSelectedDate(currentDate);
   };
 
+  const handleSubmit = async () => {
+    const response = await createTodo(
+      selectedCalendar?.id,
+      todo,
+      selectedDate,
+      color
+    );
+
+    if (response) {
+      goBack();
+    } else {
+      alert("등록 실패", "일정을 등록하는데 실패했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    setSelectedDate(new Date(date));
+  }, [date]);
+
+  if (!selectedDate || !selectedCalendar) {
+    return <LoadingComponent />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Scroll style={styles.contents}>
-        <View>
-          <TextInput
-            value={todo}
-            onChangeText={setTodo}
-            placeholder="할 일을 입력하세요."
-          />
-        </View>
-        <View style={styles.wrapper}>
-          <DateTimePicker
-            mode="date"
-            value={date}
-            onChange={handleDateChange}
-            display="spinner"
-            locale="ko-KR"
-            textColor="#000"
-          />
-        </View>
-        <View>
-          <ScheduleInput
-            icon="calendar-number-2"
-            text="Calendar 1"
-            onPress={() => {}}
-          />
-          <ScheduleInput icon="clock" text="알림" onPress={() => {}} />
-          <ScheduleInput icon="palette" text="색상" onPress={() => {}} />
-          <ScheduleInput icon="repeat" text="반복" onPress={() => {}} />
-        </View>
-      </Scroll>
-      <TextButton text="등록하기" onPress={() => {}} />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Scroll style={styles.contents}>
+          <View>
+            <TextInput
+              value={todo}
+              onChangeText={setTodo}
+              placeholder="할 일을 입력하세요."
+            />
+          </View>
+          <View style={styles.datePicker}>
+            <Text style={styles.date}>날짜: {formatDate(selectedDate)}</Text>
+            <View style={styles.wrapper}>
+              <DateTimePicker
+                mode="date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                display="spinner"
+                locale="ko-KR"
+                textColor="#000"
+              />
+            </View>
+          </View>
+          <View>
+            <ScheduleInput
+              icon="calendar-number-2"
+              text={selectedCalendar.name}
+              disabled
+            />
+            <ScheduleInput icon="palette" text="색상" onPress={openColorModal}>
+              <View style={[styles.color, { backgroundColor: color }]} />
+            </ScheduleInput>
+          </View>
+        </Scroll>
+        <TextButton text="등록하기" onPress={handleSubmit} />
+      </View>
+      <ColorModal
+        colorModalOpen={isModalOpen}
+        setColorModalOpen={setIsModalOpen}
+        selectedColor={color}
+        handleUpdateColor={handleUpdateColor}
+      />
+    </>
   );
 }
 
@@ -77,14 +136,32 @@ const createStyles = (theme: ThemeColorType) =>
     contents: {
       flex: 1,
     },
+    datePicker: {
+      gap: 8,
+    },
+    date: {
+      paddingHorizontal: 8,
+      paddingVertical: 12,
+      fontSize: 16,
+      fontWeight: "bold",
+      color: theme.highEmphasis,
+    },
     wrapper: {
       alignItems: "center",
       justifyContent: "center",
-      marginTop: 16,
-      marginHorizontal: 16,
-      paddingHorizontal: 16,
       borderRadius: 8,
       borderWidth: 1,
       borderColor: theme.border,
+    },
+    color: {
+      width: 24,
+      height: 24,
+      borderRadius: 4,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    time: {
+      fontSize: 14,
+      color: theme.mediumEmphasis,
     },
   });
