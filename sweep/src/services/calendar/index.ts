1b9cf00e78b3f7347a8bed7f4c90dc40e5b4019f
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-01-21 02:37:31 +0900

    add create schedule api

--- sweep/src/services/calendar/index.ts
+++ sweep/src/services/calendar/index.ts
@@ -7,6 +7,7 @@ import {
   toggleCalendarDaily,
   toggleCalendarNotification,
 } from "./calendars";
+import { createSchedule } from "./schedules";
 import { createTodo } from "./todos";
 
 export {
@@ -17,5 +18,6 @@ export {
   updateCalendarInfo,
   toggleCalendarDaily,
   toggleCalendarNotification,
+  createSchedule,
   createTodo,
 };
