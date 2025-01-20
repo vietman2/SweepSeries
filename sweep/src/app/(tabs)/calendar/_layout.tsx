386b1053e0eb04fd998829c7576786215bdb5e2f
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-01-21 03:20:30 +0900

    reconfigure tabs

--- sweep/src/app/(tabs)/calendar/_layout.tsx
+++ sweep/src/app/(tabs)/calendar/_layout.tsx
@@ -41,7 +41,7 @@ export default function CalendarLayout() {
           }}
         />
         <Stack.Screen
-          name="addschedule"
+          name="addschedule/[date]"
           options={{
             presentation: "modal",
             headerShown: true,
