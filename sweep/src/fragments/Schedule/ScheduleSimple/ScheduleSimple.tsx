a8eb86ac4d4d017713f0858652c009725ec5a896
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-01-22 15:25:25 +0900

    variable name change

--- sweep/src/fragments/Schedule/ScheduleSimple/ScheduleSimple.tsx
+++ sweep/src/fragments/Schedule/ScheduleSimple/ScheduleSimple.tsx
@@ -25,7 +25,11 @@ export function ScheduleSimple({ schedule }: Readonly<Props>) {
   };
 
   return (
-    <TouchableOpacity style={styles.container} onPress={handlePress} testID={`schedule-simple-${schedule.id}`}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={handlePress}
+      testID={`schedule-simple-${schedule.id}`}
+    >
       <Text style={styles.time}>{schedule.time}</Text>
       <View style={styles.horizontal}>
         <View style={[styles.chip, { backgroundColor: schedule.color }]}>
@@ -33,13 +37,10 @@ export function ScheduleSimple({ schedule }: Readonly<Props>) {
         </View>
         <VerticalDivider color={schedule.color} width={2} />
         <View style={styles.content}>
-          <Text style={styles.title}>{schedule.name}</Text>
-          {schedule.type === "레슨" && (
-            <Text style={styles.detail}>{schedule.detail}</Text>
-          )}
+          <Text style={styles.title}>{schedule.title}</Text>
+          <Text style={styles.detail}>{schedule.description}</Text>
         </View>
       </View>
-      {schedule.note && <Text style={styles.notes}>{schedule.note}</Text>}
     </TouchableOpacity>
   );
 }
