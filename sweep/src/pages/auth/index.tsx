9eb44d8ac7cfb3af6980e3a2d0463ae1dc085e98
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-01-28 23:45:36 +0900

    route

--- sweep/src/pages/auth/index.tsx
+++ sweep/src/pages/auth/index.tsx
@@ -1,6 +1,19 @@
+import { Landing } from "./Landing/Landing";
 import { Login } from "./Login/Login";
+import { Extras } from "./Register/Extras/Extras";
 import { Password } from "./Register/Password/Password";
+import { PhoneNumber } from "./Register/PhoneNumber/PhoneNumber";
+import { ReadTerm } from "./Register/Terms/ReadTerm";
 import { Terms } from "./Register/Terms/Terms";
 import { UsernameEmail } from "./Register/UsernameEmail/UsernameEmail";
 
-export { Login, Password, Terms, UsernameEmail };
+export {
+  Landing,
+  Login,
+  Extras,
+  Password,
+  PhoneNumber,
+  ReadTerm,
+  Terms,
+  UsernameEmail,
+};
