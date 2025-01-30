67e5c87141b60c6f75ea0969ed3081829d849e32
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-01-30 22:00:11 +0900

    integrate with backend

--- sweep/src/pages/mypage/EditProfile/EditProfile.test.tsx
+++ sweep/src/pages/mypage/EditProfile/EditProfile.test.tsx
@@ -1,15 +1,94 @@
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
+import * as IPicker from "expo-image-picker";
+
 import { EditProfile } from "./EditProfile";
+import * as AuthAPI from "@services/auth/auth";
+import * as ProfilesAPI from "@services/auth/profiles";
+import { sampleUser } from "@testdata/auth";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    back: jest.fn(),
+  },
+}));
 jest.mock("@fragments/Profile", () => ({
   ProfileImage: () => null,
 }));
 
 describe("<EditProfile />", () => {
-  it("renders and handles birthdate format correctly", () => {
-   const { getByTestId } = renderWithProviders(<EditProfile />);
+  const mockImage = [
+    {
+      assetId: "testId",
+      uri: "testUri",
+      width: 100,
+      height: 100,
+    },
+  ];
 
-   fireEvent.changeText(getByTestId("YYYY-MM-DD"), "1990-01-01");
+  beforeEach(() => {
+    jest.spyOn(AuthAPI, "me").mockResolvedValue(sampleUser);
+    jest.spyOn(IPicker, "launchImageLibraryAsync").mockImplementation(() =>
+      Promise.resolve({
+        canceled: false,
+        assets: mockImage,
+      })
+    );
+  });
+
+  it("handles update profile correctly", async () => {
+    jest.spyOn(AuthAPI, "me").mockResolvedValue({
+      ...sampleUser,
+      selected_profile: { nickname: null, introduction: null },
+      person: { birth_date: null },
+    });
+    jest.spyOn(ProfilesAPI, "updateProfile").mockResolvedValue(true);
+    const { getByTestId } = renderWithProviders(<EditProfile />);
+
+    await waitFor(() => {
+      fireEvent.changeText(getByTestId("nickname"), "testnickname");
+      fireEvent.changeText(getByTestId("birth"), "19910101");
+      fireEvent.changeText(getByTestId("introduction"), "testintroduction");
+      fireEvent.press(getByTestId("변경하기"));
+    });
+  });
+
+  it("handles update profile image correctly", async () => {
+    jest.spyOn(ProfilesAPI, "uploadProfileImage").mockResolvedValue(true);
+    const { getByTestId } = renderWithProviders(<EditProfile />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("profile-image"));
+    });
+  });
+
+  it("handles update profile image cancel", async () => {
+    jest.spyOn(IPicker, "launchImageLibraryAsync").mockImplementation(() =>
+      Promise.resolve({
+        canceled: true,
+        assets: null,
+      })
+    );
+    const { getByTestId } = renderWithProviders(<EditProfile />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("profile-image"));
+    });
+  });
+
+  it("handles both fail", async () => {
+    jest.spyOn(ProfilesAPI, "uploadProfileImage").mockResolvedValue(false);
+    jest.spyOn(ProfilesAPI, "updateProfile").mockResolvedValue(false);
+    const { getByTestId } = renderWithProviders(<EditProfile />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("profile-image"));
+      fireEvent.press(getByTestId("변경하기"));
+    });
+  });
+
+  it("handles profile load fail", async () => {
+    jest.spyOn(AuthAPI, "me").mockResolvedValue(null);
+    renderWithProviders(<EditProfile />);
   });
 });
--- sweep/src/pages/mypage/EditProfile/EditProfile.tsx
+++ sweep/src/pages/mypage/EditProfile/EditProfile.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+import { launchImageLibraryAsync } from "expo-image-picker";
 
 import { TextButton } from "@components/Buttons";
-import { TextInput } from "@components/Inputs";
+import { LoadingComponent } from "@components/Fallbacks";
 import { Scroll } from "@components/ScrollView";
+import { Text } from "@components/Texts";
+import { useAuth } from "@contexts/auth";
 import { useTheme } from "@contexts/theme";
 import { ProfileImage } from "@fragments/Profile";
+import { UserType } from "@models/auth";
+import { alert } from "@services/alert";
+import { me, updateProfile, uploadProfileImage } from "@services/auth";
 import { ThemeColorType } from "@themes/colors";
 import { formatBirthDate } from "@utils/formatters";
 
 export function EditProfile() {
-  //const [name, setName] = useState<string>("");
+  const [profile, setProfile] = useState<UserType>();
   const [nickname, setNickname] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
   const [birth, setBirth] = useState<string>("");
-  //const [selectedPositions, setSelectedPositions] = useState<string[]>([]);
+  const [introduction, setIntroduction] = useState<string>("");
 
+  const { selectedProfile } = useAuth();
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
@@ -25,33 +31,142 @@ export function EditProfile() {
     setBirth(formattedBirthdate);
   };
 
-  const onProfileEditPress = async () => {
-    // TODO: integrate profile edit with the backend
+  const uploadImage = async () => {
+    const result = await launchImageLibraryAsync({
+      allowsMultipleSelection: false,
+    });
+
+    if (result.canceled) return;
+
+    const image = result;
+
+    const response = await uploadProfileImage(
+      selectedProfile?.id,
+      image.assets[0]
+    );
+
+    if (response) {
+      // TODO: FINISH THIS
+      console.log("Profile image uploaded successfully");
+    }
   };
+
+  const onProfileEditPress = async () => {
+    const response = await updateProfile(
+      selectedProfile?.id,
+      nickname,
+      birth,
+      introduction
+    );
+
+    if (response) {
+      alert("프로필 변경", "프로필이 성공적으로 변경되었습니다.");
+      router.back();
+    } else {
+      alert(
+        "오류 발생",
+        "프로필 변경 중에 오류가 발생했습니다. 다시 시도해주세요."
+      );
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await me(selectedProfile?.id || null);
+
+      if (response) {
+        setProfile(response);
+        setNickname(response.selected_profile.nickname || "");
+        setBirth(response.person.birth_date || "");
+        setIntroduction(response.selected_profile.introduction || "");
+      }
+    };
+
+    fetchData();
+  }, [selectedProfile]);
+
+  if (!profile) {
+    return <LoadingComponent />;
+  }
 
   return (
     <Scroll style={styles.container} keyboardDismissMode="on-drag">
-      <ProfileImage edit />
-      <TextInput value={email} onChangeText={setEmail} />
-      <TextInput value={phone} onChangeText={setPhone} />
-      <TextInput
-        value={nickname}
-        onChangeText={setNickname}
-        placeholder={"닉네임을 입력해주세요."}
-      />
-      <TextInput
-        value={birth}
-        onChangeText={formatBirth}
-        placeholder="YYYY-MM-DD"
-        type="number-pad"
-      />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>프로필 사진</Text>
+        <TouchableOpacity onPress={uploadImage} testID="profile-image">
+          <ProfileImage
+            uri={profile.selected_profile.profile_image}
+            color={profile.selected_profile.color}
+            edit
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.wrapper}>
+        <View style={styles.row}>
+          <Text style={styles.title}>이름</Text>
+          <Text style={styles.grayText}>
+            *아카데미/코치들에게만 공개됩니다.
+          </Text>
+        </View>
+        <TextInput
+          value={profile.person.name}
+          editable={false}
+          style={[styles.textinput, styles.disabledTextInput]}
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>이메일</Text>
+        <TextInput
+          value={profile.email}
+          editable={false}
+          style={[styles.textinput, styles.disabledTextInput]}
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>연락처</Text>
+        <TextInput
+          value={profile.person.phone_number}
+          editable={false}
+          style={[styles.textinput, styles.disabledTextInput]}
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>닉네임</Text>
+        <TextInput
+          value={nickname}
+          onChangeText={setNickname}
+          placeholder="닉네임을 입력해주세요."
+          style={styles.textinput}
+          testID="nickname"
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>생년월일</Text>
+        <TextInput
+          value={birth}
+          onChangeText={formatBirth}
+          placeholder="생년월일을 입력해주세요. (YYYY-MM-DD)"
+          style={styles.textinput}
+          testID="birth"
+        />
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>자기소개</Text>
+        <TextInput
+          value={introduction}
+          onChangeText={setIntroduction}
+          placeholder="자기소개를 입력해주세요."
+          style={[styles.textinput, { height: 240 }]}
+          multiline
+          testID="introduction"
+        />
+      </View>
       <TextButton
         text="변경하기"
         backgroundColor={theme.primary}
         onPress={onProfileEditPress}
         fontSize={18}
       />
-      <View style={styles.void} />
     </Scroll>
   );
 }
@@ -64,8 +179,33 @@ const createStyles = (theme: ThemeColorType) =>
       paddingHorizontal: 16,
       paddingTop: 16,
     },
-    textInput: {
-      marginVertical: 16,
+    wrapper: {
+      marginVertical: 8,
+      gap: 8,
+    },
+    row: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    grayText: {
+      color: theme.lowEmphasis,
+      fontSize: 14,
+    },
+    textinput: {
+      height: 40,
+      paddingHorizontal: 12,
+      borderColor: theme.border,
+      borderWidth: 1,
+      borderRadius: 4,
+    },
+    disabledTextInput: {
+      color: theme.lowEmphasis,
+      backgroundColor: theme.backgroundGray,
     },
     void: {
       height: 40,
