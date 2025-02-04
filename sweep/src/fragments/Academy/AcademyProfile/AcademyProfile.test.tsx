3573ca2ef1cb367e613d38a496a07041d42d759c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-04 15:22:24 +0900

    finish academy profile page

--- sweep/src/fragments/Academy/AcademyProfile/AcademyProfile.test.tsx
+++ sweep/src/fragments/Academy/AcademyProfile/AcademyProfile.test.tsx
@@ -4,31 +4,28 @@ import { AcademyProfile } from "./AcademyProfile";
 import { sampleAcademyDetail } from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("./ImagesModal", () => ({
+  ImagesModal: () => "ImagesModal",
+}));
 jest.mock("./LogoModal", () => ({
   LogoModal: () => "LogoModal",
 }));
 
 describe("<AcademyProfile />", () => {
-  it("renders correctly", () => {
+  it("renders normal mode correctly", () => {
     renderWithProviders(<AcademyProfile academy={sampleAcademyDetail} />);
   });
 
-  it("renders pro mode and handles image modal correctly", () => {
+  it("handles pro mode modals correctly and no images", () => {
     const { getByTestId } = renderWithProviders(
-      <AcademyProfile academy={{ ...sampleAcademyDetail, images: [] }} pro />
+      <AcademyProfile
+        academy={{ ...sampleAcademyDetail, images: [] }}
+        pro
+        onRefresh={jest.fn()}
+      />
     );
 
     fireEvent.press(getByTestId("open-image-modal"));
-    fireEvent.press(getByTestId("hide"));
-    fireEvent.press(getByTestId("open-image-modal"));
-    fireEvent.press(getByTestId("저장하기"));
-  });
-
-  it("handles logo modal correctly", () => {
-    const { getByTestId } = renderWithProviders(
-      <AcademyProfile academy={sampleAcademyDetail} pro onRefresh={jest.fn()} />
-    );
-
     fireEvent.press(getByTestId("open-logo-modal"));
   });
 });
