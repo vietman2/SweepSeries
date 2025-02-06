44cfe7a7e56e6310efbd7f406d4f5c5aed075bfd
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-07 02:08:42 +0900

    route new page

--- sweep/src/pages/calendar/index.tsx
+++ sweep/src/pages/calendar/index.tsx
@@ -1,3 +1,4 @@
+import { AddLesson } from "./AddLesson/AddLesson";
 import { AddSchedule } from "./AddSchedule/AddSchedule";
 import { AddTodo } from "./AddTodo/AddTodo";
 import { Calendar } from "./Calendar/Calendar";
@@ -8,6 +9,7 @@ import { ReservationRequests } from "./ReservationRequests/ReservationRequests";
 import { Settings } from "./Settings/Settings";
 
 export {
+  AddLesson,
   AddSchedule,
   AddTodo,
   Calendar,
