4a4c3f64fb4fd7717734c42ab7ce081bb39209c1
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-06 23:46:12 +0900

    add disabled datetime header fragment

--- sweep/src/fragments/Schedule/index.tsx
+++ sweep/src/fragments/Schedule/index.tsx
@@ -1,5 +1,13 @@
-import { DateTimeHeader } from "./DateTimeHeader/DateTimeHeader";
+import {
+  DateTimeHeader,
+  DateTimeHeaderDisabled,
+} from "./DateTimeHeader/DateTimeHeader";
 import { ScheduleInput } from "./ScheduleInput/ScheduleInput";
 import { ScheduleSimple } from "./ScheduleSimple/ScheduleSimple";
 
-export { DateTimeHeader, ScheduleInput, ScheduleSimple };
+export {
+  DateTimeHeader,
+  DateTimeHeaderDisabled,
+  ScheduleInput,
+  ScheduleSimple,
+};
