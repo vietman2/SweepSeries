dc69ef212dffa039caba790756407f257ad554c2
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-07 02:45:55 +0900

    add student serializer

--- sweepseries/auth/person/serializers.py
+++ sweepseries/auth/person/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from core.utils import get_presigned_url
 from .models import Person
 
 class PersonSerializer(serializers.ModelSerializer):
@@ -14,3 +15,20 @@ class PersonSerializer(serializers.ModelSerializer):
 
     def get_phone_number(self, obj):
         return obj.phone_number.as_national
+
+class StudentSimpleSerializer(serializers.ModelSerializer):
+    phone_number = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Person
+        fields = ["id", "name", "phone_number"]
+
+    def get_phone_number(self, obj):
+        number = obj.phone_number.as_national
+
+        ## if phone number is 010-aaaa-bbbb,
+        ## return 010-xxxx-xxbb
+        if len(number) == 13:
+            return f"{number[:9]}xx{number[-2:]}"
+
+        return number
