0c1d9824d8ef100a7e781583c6cf59e5a015069c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-11 13:39:09 +0900

    finish customers page

--- sweep/src/pages/front/AcademyFront/Customers/Customers.test.tsx
+++ sweep/src/pages/front/AcademyFront/Customers/Customers.test.tsx
@@ -1,8 +1,34 @@
+import { fireEvent, waitFor } from "@testing-library/react-native";
+
 import { CustomerManagement } from "./Customers";
+import * as StudentsAPI from "@services/products/students";
+import { sampleStudents } from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    push: jest.fn(),
+  },
+}));
+jest.mock("@fragments/Profile", () => ({
+  ProfileImage: () => <div />,
+}));
+
 describe("<CustomerManagement />", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<CustomerManagement />);
+  it("renders correctly", async () => {
+    jest.spyOn(StudentsAPI, "getStudents").mockResolvedValue(sampleStudents);
+
+    const { getByTestId } = renderWithProviders(<CustomerManagement />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("refresh"));
+      fireEvent.press(getByTestId("student-1"));
+    });
+  });
+
+  it("handles api error correctly", async () => {
+    jest.spyOn(StudentsAPI, "getStudents").mockResolvedValue(null);
+
+    waitFor(() => renderWithProviders(<CustomerManagement />));
   });
 });
