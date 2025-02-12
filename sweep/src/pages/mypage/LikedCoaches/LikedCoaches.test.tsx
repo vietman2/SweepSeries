150562ade53b877fc2726fb3d4fec4fe47900e9d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-12 11:09:36 +0900

    finish page

--- sweep/src/pages/mypage/LikedCoaches/LikedCoaches.test.tsx
+++ sweep/src/pages/mypage/LikedCoaches/LikedCoaches.test.tsx
@@ -1,12 +1,34 @@
+import { fireEvent, waitFor } from "@testing-library/react-native";
+
 import { LikedCoaches } from "./LikedCoaches";
+import * as CoachesAPI from "@services/products/coach";
+import { sampleCoaches } from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    push: jest.fn(),
+  },
+}));
 jest.mock("@fragments/Coach", () => ({
   CoachSimple: () => null,
 }));
 
 describe("<LikedCoaches />", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<LikedCoaches />);
+  it("handles redirect and refresh correctly", async () => {
+    jest.spyOn(CoachesAPI, "getLikedCoaches").mockResolvedValue(sampleCoaches);
+
+    const { getByTestId } = renderWithProviders(<LikedCoaches />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("refresh"));
+      fireEvent.press(getByTestId("coach-1"));
+    });
+  });
+
+  it("handles api error", async () => {
+    jest.spyOn(CoachesAPI, "getLikedCoaches").mockResolvedValue(null);
+
+    waitFor(() => renderWithProviders(<LikedCoaches />));
   });
 });
--- sweep/src/pages/mypage/LikedCoaches/LikedCoaches.tsx
+++ sweep/src/pages/mypage/LikedCoaches/LikedCoaches.tsx
@@ -1,48 +1,81 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
 
 import { Empty } from "@components/Fallbacks";
-import { Scroll } from "@components/ScrollView";
+import { ScrollView } from "@components/ScrollView";
 import { useTheme } from "@contexts/theme";
 import { CoachSimple } from "@fragments/Coach";
 import { CoachSimpleType } from "@models/products";
-import { sampleCoaches } from "@testdata/products";
+import { getLikedCoaches } from "@services/products";
 import { ThemeColorType } from "@themes/colors";
 
 export function LikedCoaches() {
   const [coaches, setCoaches] = useState<CoachSimpleType[]>([]);
+  const [refreshCount, setRefreshCount] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
+  const handleRefresh = () => {
+    setRefreshCount((prev) => prev + 1);
+  };
+
+  const navigateToCoachDetail = (coach: CoachSimpleType) => {
+    router.push({
+      pathname: "/home/academy/coach/[id]",
+      params: { id: coach.uuid },
+    });
+  };
+
   useEffect(() => {
-    setCoaches(sampleCoaches);
-  }, []);
+    const fetchData = async () => {
+      setLoading(true);
+
+      const response = await getLikedCoaches();
+
+      if (response) {
+        setCoaches(response);
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [refreshCount]);
+
+  if (coaches.length === 0) {
+    return (
+      <Empty
+        message={"좋아요 한 코치가 없어요.\n좋아요는 코치에게 큰 힘이 됩니다."}
+      />
+    );
+  }
 
   return (
-    <>
-      {coaches.length === 0 ? (
-        <Empty
-          message={
-            "좋아요 한 코치가 없어요.\n좋아요는 코치에게 큰 힘이 됩니다."
-          }
-        />
-      ) : (
-        <Scroll style={styles.container}>
-          <View style={styles.wrapper}>
-            {coaches.map((coach) => (
-              <CoachSimple key={coach.uuid} coach={coach} />
-            ))}
-          </View>
-        </Scroll>
-      )}
-    </>
+    <View style={styles.container}>
+      <ScrollView onRefresh={handleRefresh} refreshing={loading}>
+        <View style={styles.wrapper}>
+          {coaches.map((coach) => (
+            <TouchableOpacity
+              key={coach.uuid}
+              onPress={() => navigateToCoachDetail(coach)}
+              testID={`coach-${coach.uuid}`}
+            >
+              <CoachSimple coach={coach} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const createStyles = (theme: ThemeColorType) =>
   StyleSheet.create({
     container: {
-      marginTop: 1,
+      flex: 1,
       backgroundColor: theme.background,
     },
     wrapper: {
