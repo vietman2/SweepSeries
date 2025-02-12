790090381d27bbc02b347b14fa2769365192ae65
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-12 11:24:55 +0900

    remove unnecessary tab

--- sweep/src/app/(tabs)/mypage/liked.tsx
+++ sweep/src/app/(tabs)/mypage/liked.tsx
@@ -1,4 +1,4 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createMaterialTopTabNavigator, MaterialTopTabBarProps } from "@react-navigation/material-top-tabs";
 import { Dimensions } from "react-native";
 
 import { TabBar } from "@components/Tabs";
@@ -7,16 +7,12 @@ import { LikedAcademies, LikedCoaches } from "@pages/mypage";
 const Tab = createMaterialTopTabNavigator();
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
-function Dummy() {
-  return <></>;
-}
-
 export default function RecentlyViewed() {
   return (
     <Tab.Navigator
       initialRouteName="academies"
       initialLayout={{ width: screenWidth, height: screenHeight }}
-      tabBar={(props) => <TabBar {...props} />}
+      tabBar={(props: MaterialTopTabBarProps) => <TabBar {...props} />}
       backBehavior="none"
     >
       <Tab.Screen
@@ -28,11 +24,6 @@ export default function RecentlyViewed() {
         name="coaches"
         component={LikedCoaches}
         options={{ title: "코치" }}
-      />
-      <Tab.Screen
-        name="community"
-        component={Dummy}
-        options={{ title: "커뮤니티" }}
       />
     </Tab.Navigator>
   );
