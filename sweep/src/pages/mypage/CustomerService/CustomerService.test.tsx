a2db3bf3ffc4565be79ff270afc4df2b60d15e4d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-12 13:03:51 +0900

    add inquiry form

--- sweep/src/pages/mypage/CustomerService/CustomerService.test.tsx
+++ sweep/src/pages/mypage/CustomerService/CustomerService.test.tsx
@@ -1,12 +1,19 @@
 import { Linking } from "react-native";
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
 import * as Clipboard from "expo-clipboard";
 
 import { CustomerService } from "./CustomerService";
+import * as InquiriesAPI from "@services/app/inquiries";
+import { sampleInquiries } from "@testdata/customers";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("expo-router", () => ({
+  router: {
+    back: jest.fn(),
+  },
+}));
 jest.mock("@fragments/Inquiry", () => ({
-  InquirySimple: () => null,
+  InquirySimple: () => "InquirySimple",
 }));
 
 describe("<CustomerService />", () => {
@@ -14,13 +21,42 @@ describe("<CustomerService />", () => {
     jest.spyOn(Clipboard, "setStringAsync").mockImplementationOnce(jest.fn());
   });
 
-  it("renders correctly", () => {
+  it("handles inquiry submit correctly", async () => {
     jest.spyOn(Linking, "openURL").mockImplementationOnce(jest.fn());
+    jest
+      .spyOn(InquiriesAPI, "getInquiries")
+      .mockResolvedValue(sampleInquiries);
 
     const { getByTestId } = renderWithProviders(<CustomerService />);
 
-    fireEvent.press(getByTestId("email"));
-    fireEvent.press(getByTestId("copy"));
-    fireEvent.press(getByTestId("1:1 문의하기"));
+    await waitFor(() => {
+      expect("InquirySimple").toBeTruthy();
+    });
+
+      fireEvent.press(getByTestId("email"));
+      fireEvent.press(getByTestId("copy"));
+      fireEvent.press(getByTestId("1:1 문의하기"));
+
+    jest.spyOn(InquiriesAPI, "createInquiry").mockResolvedValue(null);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("문의하기"));
+      fireEvent.changeText(getByTestId("title"), {
+        target: { value: "title" },
+      });
+      fireEvent.changeText(getByTestId("content"), {
+        target: { value: "content" },
+      });
+      fireEvent.press(getByTestId("문의하기"));
+    });
+
+    jest.spyOn(InquiriesAPI, "createInquiry").mockResolvedValueOnce(true);
+    await waitFor(() => fireEvent.press(getByTestId("문의하기")));
+  });
+
+  it("handles api error", async () => {
+    jest.spyOn(InquiriesAPI, "getInquiries").mockResolvedValueOnce(null);
+
+    renderWithProviders(<CustomerService />);
   });
 });
--- sweep/src/pages/mypage/CustomerService/CustomerService.tsx
+++ sweep/src/pages/mypage/CustomerService/CustomerService.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
-import { Linking, StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  Linking,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { router } from "expo-router";
 import * as Clipboard from "expo-clipboard";
 
 import { TextButton } from "@components/Buttons";
 import { Empty } from "@components/Fallbacks";
 import { AppIcon } from "@components/Icons";
+import { Scroll } from "@components/ScrollView";
 import { Text } from "@components/Texts";
+import { useAuth } from "@contexts/auth";
 import { useTheme } from "@contexts/theme";
 import { InquirySimple } from "@fragments/Inquiry";
 import { InquirySimpleType } from "@models/customers";
 import { alert } from "@services/alert";
-import { sampleInquiries } from "@testdata/customers";
+import { createInquiry, getInquiries } from "@services/app";
 import { ThemeColorType } from "@themes/colors";
 
 export function CustomerService() {
   const [inquiries, setInquiries] = useState<InquirySimpleType[]>([]);
+  const [fillMode, setFillMode] = useState<boolean>(false);
 
   const { theme } = useTheme();
   const styles = createStyles(theme);
@@ -32,17 +42,27 @@ export function CustomerService() {
     alert("", "이메일 주소가 복사되었습니다.");
   };
 
-  const handleAsk = () => {};
+  const handleFillMode = () => {
+    setFillMode(true);
+  };
 
   useEffect(() => {
-    setInquiries(sampleInquiries);
+    const fetchData = async () => {
+      const response = await getInquiries();
+
+      if (response) {
+        setInquiries(response);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.guide}>
         <View style={styles.contact}>
-          <AppIcon icon="envelope" size={32} color={theme.primary} />
+          <AppIcon icon="envelope" size={28} color={theme.primary} />
           <TouchableOpacity onPress={handleEmail} testID="email">
             <Text style={styles.contactText}>support@sweepseries.com</Text>
           </TouchableOpacity>
@@ -51,17 +71,94 @@ export function CustomerService() {
           </TouchableOpacity>
         </View>
         <Text style={styles.guideText}>{guideText}</Text>
-        <TextButton text="1:1 문의하기" onPress={handleAsk} />
+        {!fillMode && (
+          <TextButton text="1:1 문의하기" onPress={handleFillMode} />
+        )}
       </View>
-      {inquiries.length === 0 ? (
-        <Empty message="문의한 내역이 없습니다." color={theme.lowEmphasis} />
-      ) : (
-        <View style={styles.content}>
-          {inquiries.map((inquiry) => (
-            <InquirySimple key={inquiry.id} inquiry={inquiry} />
-          ))}
+      {fillMode ? <Form /> : <List inquiries={inquiries} />}
+    </View>
+  );
+}
+
+function Form() {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const { selectedProfile } = useAuth();
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  const handleSubmit = async () => {
+    if (!title || !content) {
+      alert("", "제목과 내용을 입력해주세요.");
+      return;
+    }
+
+    const response = await createInquiry(title, content);
+
+    if (response) {
+      alert("", "문의가 성공적으로 등록되었습니다.");
+      router.back();
+    } else {
+      alert("", "문의 등록에 실패했습니다.");
+    }
+  };
+
+  return (
+    <Scroll>
+      <View style={styles.form}>
+        <View style={styles.wrapper}>
+          <Text style={styles.subtitle}>고객정보</Text>
+          <TextInput
+            value={selectedProfile?.name}
+            style={[styles.textinput, styles.disabled]}
+            editable={false}
+          />
         </View>
-      )}
+        <View style={styles.wrapper}>
+          <Text style={styles.subtitle}>문의하기</Text>
+          <TextInput
+            value={title}
+            onChangeText={setTitle}
+            placeholder="제목을 입력해주세요."
+            style={styles.textinput}
+            testID="title"
+          />
+          <TextInput
+            value={content}
+            onChangeText={setContent}
+            placeholder="내용을 입력해주세요."
+            style={[styles.textinput, styles.large]}
+            multiline
+            numberOfLines={4}
+            testID="content"
+          />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <TextButton text="문의하기" onPress={handleSubmit} />
+        </View>
+      </View>
+    </Scroll>
+  );
+}
+
+interface Props {
+  inquiries: InquirySimpleType[];
+}
+
+function List({ inquiries }: Readonly<Props>) {
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  if (inquiries.length === 0) {
+    return <Empty message="문의 내역이 없습니다." color={theme.lowEmphasis} />;
+  }
+
+  return (
+    <View style={styles.content}>
+      {inquiries.map((inquiry) => (
+        <InquirySimple key={inquiry.id} inquiry={inquiry} />
+      ))}
     </View>
   );
 }
@@ -82,7 +179,7 @@ const createStyles = (theme: ThemeColorType) =>
       gap: 16,
     },
     contactText: {
-      fontSize: 24,
+      fontSize: 20,
       fontWeight: "bold",
       color: theme.primary,
     },
@@ -94,5 +191,36 @@ const createStyles = (theme: ThemeColorType) =>
     content: {
       flex: 1,
       backgroundColor: theme.background,
+    },
+    form: {
+      flex: 1,
+      paddingHorizontal: 24,
+      paddingVertical: 16,
+      gap: 16,
+      backgroundColor: theme.background,
+    },
+    wrapper: {
+      gap: 12,
+    },
+    subtitle: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: theme.highEmphasis,
+    },
+    textinput: {
+      height: 40,
+      padding: 8,
+      borderWidth: 1,
+      borderColor: theme.border,
+      borderRadius: 4,
+    },
+    disabled: {
+      backgroundColor: theme.backgroundGray,
+    },
+    large: {
+      height: 200,
+    },
+    buttonWrapper: {
+      marginVertical: 8,
     },
   });
