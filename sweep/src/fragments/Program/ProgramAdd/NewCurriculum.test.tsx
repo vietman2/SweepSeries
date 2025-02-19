5841e22e17ba05196760d756ae32dab831074a0b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-19 13:41:38 +0900

    refactor

---
+++ sweep/src/fragments/Program/ProgramAdd/NewCurriculum.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent } from "@testing-library/react-native";
+
+import { NewCurriculum } from "./NewCurriculum";
+import { sampleCurriculums } from "@testdata/products";
+import { renderWithProviders } from "@utils/test-utils";
+
+describe("<NewCurriculum />", () => {
+  it("should render single row", () => {
+    const { getByTestId } = renderWithProviders(
+      <NewCurriculum rows={[sampleCurriculums[0]]} setRows={jest.fn()} />
+    );
+
+    fireEvent.press(getByTestId("plus"));
+    fireEvent.changeText(getByTestId("수업 수"), "10");
+    fireEvent.changeText(getByTestId("가격"), "10000");
+    fireEvent.press(getByTestId("delete-button0"));
+  });
+
+  it("should render multiple rows", () => {
+    const { getByTestId, getAllByTestId } = renderWithProviders(
+      <NewCurriculum rows={sampleCurriculums} setRows={jest.fn()} />
+    );
+
+    fireEvent.changeText(getAllByTestId("수업 수")[0], "");
+    fireEvent.press(getByTestId("delete-button0"));
+  });
+});
--- sweep/src/fragments/Program/index.tsx
+++ sweep/src/fragments/Program/index.tsx
@@ -1,4 +1,5 @@
 import { CurriculumChip } from "./Curriculum/Curriculum";
+import { NewCurriculum } from "./ProgramAdd/NewCurriculum";
 import { ProgramSimple } from "./ProgramSimple/ProgramSimple";
 
-export { CurriculumChip, ProgramSimple };
+export { CurriculumChip, NewCurriculum, ProgramSimple };
