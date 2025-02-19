8e14fdfce43789e0a05263c3ecc4f96542ccb68f
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-19 14:07:44 +0900

    add contract model

--- sweepseries/sweepseries/settings/base.py
+++ sweepseries/sweepseries/settings/base.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     'product.academy.apps.AcademyConfig',
     'product.address.apps.AddressConfig',
     'product.coach.apps.CoachConfig',
+    'product.contract.apps.ContractConfig',
     'product.program.apps.ProgramConfig',
 ]
 
