c2b508d238271e231d309eea103ee867a7226aa7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-25 08:59:02 +0900

    update test data

--- sweep/src/constants/testdata/calendar/calendars.ts
+++ sweep/src/constants/testdata/calendar/calendars.ts
@@ -11,7 +11,7 @@ export const sampleCalendars: CalendarType[] = [
     role: "owner",
     logo: "https://picsum.photos/200/200",
     num_members: 1,
-    type: "PERSONAL",
+    type: "personal",
   },
   {
     uuid: "2",
@@ -21,6 +21,6 @@ export const sampleCalendars: CalendarType[] = [
     notifications_today: false,
     daily_time: "",
     role: "owner",
-    type: "ACADEMY",
+    type: "academy",
   },
 ];
