af1c3ada3b72d287aa777e97fe6723148fdc81fa
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-25 10:27:04 +0900

    update calendar fragments

--- sweep/src/fragments/Calendar/ColorModal/ColorModal.test.tsx
+++ sweep/src/fragments/Calendar/ColorModal/ColorModal.test.tsx
@@ -1,16 +1,32 @@
 import { fireEvent } from "@testing-library/react-native";
 
 import { ColorModal } from "./ColorModal";
+import * as CalendarsAPI from "@services/calendar/calendars";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<ColorModal />", () => {
-  it("renders", () => {
+  it("handles color update", () => {
+    jest.spyOn(CalendarsAPI, "updateCalendarInfo").mockResolvedValue(true);
+
     const { getByTestId } = renderWithProviders(
       <ColorModal
         colorModalOpen
         setColorModalOpen={jest.fn()}
         selectedColor="#FF6B6B"
-        handleUpdateColor={jest.fn()}
+      />
+    );
+
+    fireEvent.press(getByTestId("select-color-#FF6B6B"));
+  });
+
+  it("handles api error", () => {
+    jest.spyOn(CalendarsAPI, "updateCalendarInfo").mockResolvedValue(null);
+
+    const { getByTestId } = renderWithProviders(
+      <ColorModal
+        colorModalOpen
+        setColorModalOpen={jest.fn()}
+        selectedColor="#FF6B6B"
       />
     );
 
--- sweep/src/fragments/Calendar/index.tsx
+++ sweep/src/fragments/Calendar/index.tsx
@@ -1,19 +1,17 @@
 import { AlarmModal } from "./AlarmModal/AlarmModal";
 import { CalendarButtons } from "./CalendarButtons/CalendarButtons";
-import { CalendarMembers } from "./CalendarMembers/CalendarMembers";
-import { CalendarOptions } from "./CalendarOptions/CalendarOptions";
 import { CalendarSimple } from "./CalendarSimple/CalendarSimple";
 import { CalendarTitle } from "./CalendarTitle/CalendarTitle";
 import { ColorModal } from "./ColorModal/ColorModal";
+import { NameModal } from "./NameModal/NameModal";
 import { RepeatModal } from "./RepeatModal/RepeatModal";
 
 export {
   AlarmModal,
   CalendarButtons,
-  CalendarMembers,
-  CalendarOptions,
   CalendarSimple,
   CalendarTitle,
   ColorModal,
+  NameModal,
   RepeatModal,
 };
