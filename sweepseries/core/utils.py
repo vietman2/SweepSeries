45d1be2f1009a772eabdc898b176206bee19395e
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-25 15:23:25 +0900

    refactor methods

--- sweepseries/core/utils.py
+++ sweepseries/core/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils import timezone
 import boto3
 
 s3 = boto3.client(
@@ -23,3 +24,30 @@ def get_presigned_url(filename):
 def is_admin_page(request):
     admin_page_url = settings.ADMIN_PAGE_URL
     return request.META.get('HTTP_ORIGIN') == admin_page_url
+
+def get_time_text(time):
+    ampm = '오전'
+    local_time = timezone.localtime(time)
+    ## return in correct timezones
+    hour = local_time.strftime('%H')
+    minute = local_time.strftime('%M')
+
+    if hour > '12':
+        ampm = '오후'
+        hour = int(hour) - 12
+
+    if minute == '00':
+        return f'{ampm} {hour}시'
+
+    return f'{ampm} {hour}시 {minute}분'
+
+def get_duration_text(duration):
+    days = duration.days
+    hours, remainder = divmod(duration.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    days_text = f'{days}일' if days else ''
+    hours_text = f'{hours}시간' if hours else ''
+    minutes_text = f'{minutes}분' if minutes else ''
+
+    return ' '.join([text for text in [days_text, hours_text, minutes_text] if text])
