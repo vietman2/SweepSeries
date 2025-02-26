3dc9dc3aaa772b73cca9eceb53c42e38354a4854
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-26 16:03:40 +0900

    minor fix

--- sweep/src/fragments/Program/ProgramAdd/CurriculumModal.test.tsx
+++ sweep/src/fragments/Program/ProgramAdd/CurriculumModal.test.tsx
@@ -4,6 +4,10 @@ import { CurriculumModal } from "./CurriculumModal";
 import { sampleCurriculums } from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("./NewCurriculum", () => ({
+  NewCurriculum: () => null,
+}));
+
 describe("<CurriculumModal />", () => {
   it("renders without crashing", () => {
     const { getByTestId } = renderWithProviders(
