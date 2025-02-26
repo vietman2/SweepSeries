5a9f030df871d7d9e8e67f7b024ac778e6986601
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-26 17:12:11 +0900

    change program create api and add edit logics

--- sweepseries/product/program/serializers.py
+++ sweepseries/product/program/serializers.py
@@ -1,7 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from rest_framework import serializers
 
-from .models import Program, Position, Target, Curriculum
+from product.academy.models import Academy
+from product.coach.models import Coach
+from product.coach.serializers import CoachSimpleSerializer
+from .models import Program, Position, Target, Curriculum, CoachTeam
+from .utils import create_curriculum
 
 class TargetSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,28 +23,55 @@ class CurriculumSerializer(serializers.ModelSerializer):
         model = Curriculum
         fields = ["id", "num_lessons", "price"]
 
-    def validate_num_lessons(self, value):
-        if value < 1:
-            raise serializers.ValidationError("수업 횟수는 1보다 작을 수 없습니다.")
-        return value
+class CoachTeamSerializer(serializers.ModelSerializer):
+    id      = serializers.UUIDField(read_only=True)
+    coaches = serializers.SerializerMethodField()
+    uuids   = serializers.ListField(child=serializers.UUIDField(), write_only=True)
 
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("가격은 0보다 작을 수 없습니다.")
-        return value
+    class Meta:
+        model = CoachTeam
+        fields = ["id", "coaches", "uuids"]
+
+    def get_coaches(self, obj):
+        coaches = obj.coaches.all()
+
+        return CoachSimpleSerializer(coaches, many=True).data
+
+    def create(self, validated_data):
+        program = self.context["program"]
+        uuids = validated_data.pop("uuids")
+        coach_team = CoachTeam.objects.create()
+
+        for uuid in uuids:
+            coach = program.academy.coaches.get(uuid=uuid)
+            coach_team.coaches.add(coach)
+
+        program.teams.add(coach_team)
+
+        return coach_team
 
 class ProgramSerializer(serializers.ModelSerializer):
-    target          = TargetSerializer(read_only=True)
-    positions       = PositionSerializer(many=True, read_only=True)
-    curriculums     = CurriculumSerializer(many=True)
-    lowest_price    = serializers.SerializerMethodField()
-    academy_uuid    = serializers.SerializerMethodField()
+    target              = TargetSerializer(read_only=True)
+    positions           = PositionSerializer(many=True, read_only=True)
+    curriculums         = serializers.SerializerMethodField()
+    lowest_price        = serializers.SerializerMethodField()
+    academy_uuid        = serializers.SerializerMethodField()
+    random_assignment   = serializers.SerializerMethodField()
+    teams               = serializers.SerializerMethodField()
+    academy             = serializers.UUIDField(write_only=True)
+    target_id           = serializers.IntegerField(write_only=True)
+    positions_id        = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    curriculum_data     = serializers.ListField(write_only=True)
+    coach_team          = serializers.JSONField(write_only=True)
 
     class Meta:
         model = Program
         fields = [
             "id", "name", "duration", "target", "positions",
-            "curriculums", "lowest_price", "academy_uuid"
+            "curriculums", "lowest_price", "academy_uuid",
+            "random_assignment", "teams",
+            "academy", "target_id", "positions_id",
+            "curriculum_data", "coach_team"
         ]
 
     def get_lowest_price(self, obj):
@@ -48,22 +80,97 @@ class ProgramSerializer(serializers.ModelSerializer):
     def get_academy_uuid(self, obj):
         return obj.academy.uuid
 
+    def get_curriculums(self, obj):
+        curriculums = obj.curriculums.filter(is_deleted=False)
+
+        return CurriculumSerializer(curriculums, many=True).data
+
+    def get_random_assignment(self, obj):
+        return obj.select_disabled
+
+    def get_teams(self, obj):
+        if obj.select_disabled:
+            return []
+
+        return CoachTeamSerializer(obj.teams.all(), many=True).data
+
     def validate_duration(self, value):
         if value % 30 != 0:
             raise serializers.ValidationError("수업 시간은 30분 단위로 입력해주세요.")
 
         return value
 
-    def save(self, **kwargs):
+    def validate_academy(self, value):
+        try:
+            academy = Academy.objects.get(uuid=value)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError("존재하지 않는 아카데미입니다.")
+
+        return academy
+
+    def set_team(self, program, coach_team):
+        if coach_team['select_disabled']:
+            program.select_disabled = True
+            program.save()
+
+            return program
+
+        teams = coach_team['teams']
+
+        if not teams:
+            raise serializers.ValidationError("팀을 선택해주세요.")
+
+        for team in teams:
+            coaches = team['coaches']
+            new_team = CoachTeam.objects.create()
+            for coach in coaches:
+                coach_obj = Coach.objects.get(uuid=coach['uuid'])
+                new_team.coaches.add(coach_obj)
+
+            program.teams.add(new_team)
+
+        program.select_disabled = False
+        program.save()
+
+        return program
+
+    def update(self, instance, validated_data):
+        target_id = validated_data.get("target_id", instance.target_id)
+        positions_id = validated_data.get("positions_id")
+
+        name = validated_data.get("name", instance.name)
+        duration = validated_data.get("duration", instance.duration)
+        target = Target.objects.get(id=target_id)
+        positions = Position.objects.filter(id__in=positions_id)
+
+        instance.name = name
+        instance.duration = duration
+        instance.target = target
+        instance.positions.set(positions)
+
+        instance.save()
+
+        return instance
+
+    def create(self, validated_data):
         with transaction.atomic():
-            academy = kwargs.pop("academy")
-            curriculums = self.validated_data.pop("curriculums")
-            target = kwargs.pop("target")
-            positions = kwargs.pop("positions")
-            program = Program.objects.create(academy=academy, target=target, **self.validated_data)
+            curriculums = validated_data.pop("curriculum_data")
+            target_id = validated_data.pop("target_id")
+            positions_id = validated_data.pop("positions_id")
+            target = Target.objects.get(id=target_id)
+            validated_data["target"] = target
+            coach_team_data = validated_data.pop("coach_team")
+
+            program = Program.objects.create(**validated_data)
+
+            positions = Position.objects.filter(id__in=positions_id)
 
             program.positions.set(positions)
             for curriculum in curriculums:
-                Curriculum.objects.create(program=program, **curriculum)
+                num_lessons = curriculum["num_lessons"]
+                price = curriculum["price"]
+                program = create_curriculum(program, num_lessons, price)
+
+            program = self.set_team(program, coach_team_data)
 
             return program
--- sweepseries/product/program/tests.py
+++ sweepseries/product/program/tests.py
@@ -19,15 +19,29 @@ class ProgramTestCase(APITestCase):
         self.data = {
             "name": "프로그램 1",
             "academy": str(self.academy.uuid),
-            "target": 1,
-            "positions": [1, 2],
+            "target_id": 1,
+            "positions_id": [1, 2],
             "duration": "60",
-            "curriculums": [
+            "curriculum_data": [
                 {
                     "num_lessons": 10,
                     "price": 100000,
                 }
-            ]
+            ],
+            "coach_team": {
+                "select_disabled": True,
+                "teams": [{
+                    "coaches": [
+                        {"uuid": "923e4567-e89b-12d3-a456-426614174999"},
+                    ]
+                }]
+            }
+        }
+        self.edit_data = {
+            "name": "프로그램 1",
+            "target_id": 1,
+            "positions_id": [1, 2],
+            "duration": "60",
         }
 
     def test_targets(self):
@@ -64,8 +78,20 @@ class ProgramTestCase(APITestCase):
         response = self.client.get(f"{self.url}?profile=999")
         self.assertEqual(response.status_code, 404)
 
+    def test_retrieve(self):
+        response = self.client.get(f"{self.url}1/")
+        self.assertEqual(response.status_code, 200)
+
     def test_create(self):
+        ## 1. select disabled
         response = self.client.post(self.url, data=self.data, format="json")
+        self.assertEqual(response.status_code, 201)
+
+        ## 2. select enabled
+        data = self.data.copy()
+        data["name"] = "프로그램 2"
+        data["coach_team"]["select_disabled"] = False
+        response = self.client.post(self.url, data=data, format="json")
         self.assertEqual(response.status_code, 201)
 
     def test_create_fail(self):
@@ -80,9 +106,146 @@ class ProgramTestCase(APITestCase):
         response = self.client.post(self.url, data=data, format="json")
         self.assertEqual(response.status_code, 400)
 
-        ## 3. bad data (bad num_lessons and price)
+        ## 3. bad data (invalid num lessons)
         data = self.data.copy()
-        data["curriculums"][0]["num_lessons"] = 0
-        data["curriculums"][0]["price"] = -100000
+        data["curriculum_data"] = [
+            {
+                "num_lessons": 0,
+                "price": 100000,
+            }
+        ]
         response = self.client.post(self.url, data=data, format="json")
         self.assertEqual(response.status_code, 400)
+
+        ## 4. bad data (invalid price)
+        data = self.data.copy()
+        data["curriculum_data"] = [
+            {
+                "num_lessons": 10,
+                "price": -100000,
+            }
+        ]
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+        ## 5. bad data (no academy)
+        data = self.data.copy()
+        data["academy"] = "923e4567-e89b-12d3-a456-426614174999"
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+        ## 6. bad data (no team)
+        data = self.data.copy()
+        data["coach_team"]["select_disabled"] = False
+        data["coach_team"]["teams"] = []
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit(self):
+        response = self.client.patch(f"{self.url}1/", data=self.edit_data, format="json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_fail(self):
+        ## 1. bad data (bad duration)
+        data = self.edit_data.copy()
+        data["duration"] = "45"
+        response = self.client.patch(f"{self.url}1/", data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete(self):
+        response = self.client.delete(f"{self.url}1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_curriculums(self):
+        data = {
+            "curriculums": [
+                {
+                    "num_lessons": 1,
+                    "price": 100000,
+                },
+                {
+                    "num_lessons": 5,
+                    "price": 450000,
+                },
+            ]
+        }
+
+        response = self.client.patch(f"{self.url}1/curriculums/", data=data, format="json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_curriculums_fail(self):
+        ## 1. no data
+        response = self.client.patch(f"{self.url}1/curriculums/")
+        self.assertEqual(response.status_code, 400)
+
+        ## 2. bad data (invalid num lessons)
+        data = {
+            "curriculums": [
+                {
+                    "num_lessons": 0,
+                    "price": 100000,
+                },
+            ]
+        }
+        response = self.client.patch(f"{self.url}1/curriculums/", data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+        ## 3. bad data (invalid price)
+        data = {
+            "curriculums": [
+                {
+                    "num_lessons": 1,
+                    "price": -100000,
+                },
+            ]
+        }
+        response = self.client.patch(f"{self.url}1/curriculums/", data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_toggle(self):
+        response = self.client.patch(f"{self.url}1/toggle/")
+        self.assertEqual(response.status_code, 200)
+
+class CoachTeamTestCase(APITestCase):
+    fixtures = [
+        "core/data/test/users.json", "core/data/initial/regions.json",
+        "core/data/test/academies.json", "core/data/test/coaches.json",
+        "core/data/test/programs.json", "core/data/initial/facilities.json",
+        "core/data/initial/programs.json", "core/data/initial/professions.json"
+    ]
+
+    def setUp(self):
+        self.url = "/v1/programs/1/coaches/"
+        self.user = User.objects.get(username="normaluser")
+        self.userprofile = self.user.profiles.first()
+        self.academy = Academy.objects.get(name="아카데미 1")
+        self.data = {
+            "uuids": ["923e4567-e89b-12d3-a456-426614174999"],
+        }
+
+    def test_create(self):
+        response = self.client.post(self.url, data=self.data, format="json")
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_fail(self):
+        ## 1. program does not exist
+        response = self.client.post("/v1/programs/999/coaches/", data={}, format="json")
+        self.assertEqual(response.status_code, 404)
+
+        ## 2. no data
+        response = self.client.post(self.url, data={}, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete(self):
+        self.client.post(self.url, data=self.data, format="json")
+        response = self.client.delete(f"{self.url}1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_fail(self):
+        ## 1. program does not exist
+        response = self.client.delete("/v1/programs/999/coaches/1/")
+        self.assertEqual(response.status_code, 404)
+
+        ## 2. team does not exist
+        response = self.client.delete(f"{self.url}999/")
+        self.assertEqual(response.status_code, 404)
