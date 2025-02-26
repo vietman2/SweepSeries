72aa7965e99014ea7b86b698374878038f2369b7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-26 15:50:38 +0900

    add curriculum modal

---
+++ sweep/src/fragments/Program/ProgramAdd/CurriculumModal.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent } from "@testing-library/react-native";
+
+import { CurriculumModal } from "./CurriculumModal";
+import { sampleCurriculums } from "@testdata/products";
+import { renderWithProviders } from "@utils/test-utils";
+
+describe("<CurriculumModal />", () => {
+  it("renders without crashing", () => {
+    const { getByTestId } = renderWithProviders(
+      <CurriculumModal
+        curriculums={sampleCurriculums}
+        closeModal={jest.fn()}
+        submit={jest.fn()}
+      />
+    );
+
+    fireEvent.press(getByTestId("저장"));
+  });
+});
