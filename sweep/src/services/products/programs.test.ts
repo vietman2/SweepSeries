17934901909b63278f4826fcd07435355bd4f9cc
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-26 16:03:14 +0900

    add new apis

--- sweep/src/services/products/programs.test.ts
+++ sweep/src/services/products/programs.test.ts
@@ -6,7 +6,15 @@ import {
   getPrograms,
   createProgram,
   getProgramsByProfile,
+  deleteProgram,
+  editProgram,
+  getProgramDetail,
+  addCoachTeam,
+  deleteCoachTeam,
+  saveCurriculums,
+  toggleCoachSelect,
 } from "./programs";
+import { sampleCurriculums } from "@testdata/products";
 
 describe("getTargets", () => {
   it("should return the targets", async () => {
@@ -93,6 +101,138 @@ describe("getProgramsByProfile", () => {
 
   it("should return null if the profile is undefined", async () => {
     const result = await getProgramsByProfile(undefined);
+    expect(result).toBeNull();
+  });
+});
+
+describe("deleteProgram", () => {
+  it("should delete the program", async () => {
+    jest.spyOn(axios, "delete").mockResolvedValue({ data: {} });
+    const result = await deleteProgram(1);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "delete").mockRejectedValue(new Error());
+    const result = await deleteProgram(1);
+    expect(result).toBeNull();
+  });
+
+  it("should return null if the program is undefined", async () => {
+    const result = await deleteProgram(undefined);
+    expect(result).toBeNull();
+  });
+});
+
+describe("editProgram", () => {
+  it("should edit the program", async () => {
+    jest.spyOn(axios, "patch").mockResolvedValue({ data: {} });
+    const result = await editProgram(1, "name", 1, 1, [1]);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+    const result = await editProgram(1, "name", 1, 1, [1]);
+    expect(result).toBeNull();
+  });
+
+  it("should return null if the program is undefined", async () => {
+    const result = await editProgram(undefined, "name", 1, 1, [1]);
+    expect(result).toBeNull();
+  });
+});
+
+describe("getProgramDetail", () => {
+  it("should return the program detail", async () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: {} });
+    const result = await getProgramDetail("1");
+    expect(result).toEqual({});
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error());
+    const result = await getProgramDetail("1");
+    expect(result).toBeNull();
+  });
+});
+
+describe("addCoachTeam", () => {
+  it("should add a coach to the team", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue({ data: {} });
+    const result = await addCoachTeam(1, ["1"]);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if programid is undefined", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue({ data: {} });
+    const result = await addCoachTeam(undefined, ["1"]);
+    expect(result).toEqual(null);
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "post").mockRejectedValue(new Error());
+    const result = await addCoachTeam(1, ["1"]);
+    expect(result).toBeNull();
+  });
+});
+
+describe("deleteCoachTeam", () => {
+  it("should delete a coach from the team", async () => {
+    jest.spyOn(axios, "delete").mockResolvedValue({ data: {} });
+    const result = await deleteCoachTeam(1, 1);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if the programid is undefined", async () => {
+    jest.spyOn(axios, "delete").mockRejectedValue(new Error());
+    const result = await deleteCoachTeam(undefined, 1);
+    expect(result).toBeNull();
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "delete").mockRejectedValue(new Error());
+    const result = await deleteCoachTeam(1, 1);
+    expect(result).toBeNull();
+  });
+});
+
+describe("saveCurriculums", () => {
+  it("should save the curriculums", async () => {
+    jest.spyOn(axios, "patch").mockResolvedValue({ data: {} });
+    const result = await saveCurriculums(1, sampleCurriculums);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if programid is undefined", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+    const result = await saveCurriculums(undefined, sampleCurriculums);
+    expect(result).toBeNull();
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+    const result = await saveCurriculums(1, sampleCurriculums);
+    expect(result).toBeNull();
+  });
+});
+
+describe("toggleCoachSelect", () => {
+  it("should toggle the coach select", async () => {
+    jest.spyOn(axios, "patch").mockResolvedValue({ data: {} });
+    const result = await toggleCoachSelect(1);
+    expect(result).toEqual({});
+  });
+
+  it("should return null if programid is undefined", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+    const result = await toggleCoachSelect(undefined);
+    expect(result).toBeNull();
+  });
+
+  it("should return null if the request fails", async () => {
+    jest.spyOn(axios, "patch").mockRejectedValue(new Error());
+    const result = await toggleCoachSelect(1);
     expect(result).toBeNull();
   });
 });
--- sweep/src/services/products/programs.ts
+++ sweep/src/services/products/programs.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { CurriculumType } from "@models/products";
+
 export async function getTargets() {
   try {
     const response = await axios.get("/v1/programs/targets/");
@@ -22,8 +24,8 @@ export async function createProgram(
   academy_id: string,
   name: string,
   duration: number,
-  target: number,
-  positions: number[],
+  target_id: number,
+  positions_id: number[],
   curriculums: {
     num_lessons: number;
     price: number;
@@ -34,9 +36,46 @@ export async function createProgram(
       academy: academy_id,
       name,
       duration,
-      target,
-      positions,
+      target_id,
+      positions_id,
       curriculums,
+    });
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function deleteProgram(program_id: number | undefined) {
+  if (!program_id) {
+    return null;
+  }
+
+  try {
+    const response = await axios.delete(`/v1/programs/${program_id}/`);
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function editProgram(
+  program_id: number | undefined,
+  name: string,
+  duration: number,
+  target_id: number,
+  positions_id: number[]
+) {
+  if (!program_id) {
+    return null;
+  }
+
+  try {
+    const response = await axios.patch(`/v1/programs/${program_id}/`, {
+      name,
+      duration,
+      target_id,
+      positions_id,
     });
     return response.data;
   } catch {
@@ -60,6 +99,88 @@ export async function getProgramsByProfile(profile_id: number | undefined) {
 
   try {
     const response = await axios.get(`/v1/programs/?profile=${profile_id}`);
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function getProgramDetail(id: string) {
+  try {
+    const response = await axios.get(`/v1/programs/${id}/`);
+
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function addCoachTeam(
+  programId: number | undefined,
+  coach_uuids: string[]
+) {
+  if (!programId) {
+    return null;
+  }
+
+  try {
+    const response = await axios.post(`/v1/programs/${programId}/coaches/`, {
+      uuids: coach_uuids,
+    });
+
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function deleteCoachTeam(
+  programId: number | undefined,
+  teamId: number
+) {
+  if (!programId) {
+    return null;
+  }
+
+  try {
+    const response = await axios.delete(
+      `/v1/programs/${programId}/coaches/${teamId}/`
+    );
+
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function saveCurriculums(
+  programId: number | undefined,
+  curriculums: CurriculumType[]
+) {
+  if (!programId) {
+    return null;
+  }
+
+  try {
+    const response = await axios.patch(
+      `/v1/programs/${programId}/curriculums/`,
+      { curriculums }
+    );
+
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function toggleCoachSelect(programId: number | undefined) {
+  if (!programId) {
+    return null;
+  }
+
+  try {
+    const response = await axios.patch(`/v1/programs/${programId}/toggle/`);
+
     return response.data;
   } catch {
     return null;
