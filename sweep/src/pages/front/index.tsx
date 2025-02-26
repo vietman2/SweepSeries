29cb03bf2e9b58f642f68940b112b499d1d7601d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-26 16:33:51 +0900

    route new pages

--- sweep/src/pages/front/index.tsx
+++ sweep/src/pages/front/index.tsx
@@ -2,5 +2,6 @@ import { CreateProgram } from "./CreateProgram/CreateProgram";
 import { CustomerDetail } from "./CustomerDetail/CustomerDetail";
 import { Front } from "./FrontMain/FrontMain";
 import { NoticeEdit } from "./NoticeEdit/NoticeEdit";
+import { ProgramEdit } from "./ProgramEdit/ProgramEdit";
 
-export { CreateProgram, CustomerDetail, Front, NoticeEdit };
+export { CreateProgram, CustomerDetail, Front, NoticeEdit, ProgramEdit };
