c04eadd463ff27eb6cf933bc831d1707a3dca717
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-21 04:10:55 +0900

    remove old fragments and route new

--- sweep/src/fragments/Academy/AcademySuggest/AcademySuggest.test.tsx
+++
@@ -1,9 +0,0 @@
-import { AcademySuggest } from "./AcademySuggest";
-import { sampleAcademies } from "@testdata/products";
-import { renderWithProviders } from "@utils/test-utils";
-
-describe("<AcademySuggest />", () => {
-  it("renders correctly", () => {
-    renderWithProviders(<AcademySuggest academy={sampleAcademies[0]} />);
-  });
-});
