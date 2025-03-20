4f10a07900e0109219cfb38c4c8117a8bc820af7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-21 07:38:04 +0900

    remove unused components and route new pages

--- sweep/src/pages/home/index.tsx
+++ sweep/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
+import { HomeLayout } from "./_layout/HomeLayout";
 import { AcademyDetail } from "./AcademyDetail/AcademyDetail";
 import { CoachDetail } from "./AcademyDetail/CoachDetail/CoachDetail";
-import { Home } from "./Home/Home";
+import { Home } from "./Home";
 import { MyAcademy } from "./MyAcademy/MyAcademy";
 import { NoticeDetail } from "./NoticeDetail/NoticeDetail";
 import { Reservation } from "./Reservation/Reservation";
 
 export {
+  HomeLayout,
   AcademyDetail,
   CoachDetail,
   MyAcademy,
