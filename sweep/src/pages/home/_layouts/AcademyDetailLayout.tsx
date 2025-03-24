bd40ac0b5bf63169109bced065bf920d862d7840
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-24 17:58:50 +0900

    add academy detail layout

---
+++ sweep/src/pages/home/_layouts/AcademyDetailLayout.tsx
@@ -0,0 +1,136 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Slot, router, usePathname } from "expo-router";
+
+import { Scroll } from "@components/ScrollView";
+import { AcademyDetailProvider, useAcademyDetail } from "@contexts/academy";
+import { useTheme } from "@contexts/theme";
+import { AcademyProfile } from "@fragments/Academy";
+import { ThemeColorType } from "@themes/colors";
+
+export function AcademyDetailLayout() {
+  return (
+    <AcademyDetailProvider>
+      <AcademyDetail />
+    </AcademyDetailProvider>
+  );
+}
+
+type AcademyDetailTabType = {
+  name: string;
+  pathname:
+    | "/home/academy/[id]/information"
+    | "/home/academy/[id]/programs"
+    | "/home/academy/[id]/coaches"
+    | "/home/academy/[id]/notices"
+    | "/home/academy/[id]/reviews";
+  relativePath: string;
+};
+
+const tabs: AcademyDetailTabType[] = [
+  {
+    name: "소개",
+    pathname: "/home/academy/[id]/information",
+    relativePath: "information",
+  },
+  {
+    name: "프로그램",
+    pathname: "/home/academy/[id]/programs",
+    relativePath: "programs",
+  },
+  {
+    name: "코치",
+    pathname: "/home/academy/[id]/coaches",
+    relativePath: "coaches",
+  },
+  {
+    name: "소식",
+    pathname: "/home/academy/[id]/notices",
+    relativePath: "notices",
+  },
+  {
+    name: "리뷰",
+    pathname: "/home/academy/[id]/reviews",
+    relativePath: "reviews",
+  },
+];
+
+function AcademyDetail() {
+  const { academy } = useAcademyDetail();
+  const pathname = usePathname();
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  const isTabActive = (tab: AcademyDetailTabType) => {
+    return pathname.includes(tab.relativePath);
+  };
+
+  if (!academy) {
+    return null;
+  }
+
+  const handleTabPress = (tab: AcademyDetailTabType) => {
+    router.replace({
+      pathname: tab.pathname,
+      params: { id: academy?.uuid },
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Scroll>
+        <AcademyProfile academy={academy} />
+        <View style={styles.tabs}>
+          {tabs.map((tab) => (
+            <TouchableOpacity
+              key={tab.name}
+              style={[styles.tab, isTabActive(tab) && styles.selectedTab]}
+              onPress={() => handleTabPress(tab)}
+              testID={`tab-${tab.relativePath}`}
+            >
+              <Text
+                style={[
+                  styles.tabText,
+                  isTabActive(tab) && styles.selectedTabText,
+                ]}
+              >
+                {tab.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Slot />
+      </Scroll>
+    </View>
+  );
+}
+
+const createStyles = (theme: ThemeColorType) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    tabs: {
+      flexDirection: "row",
+      backgroundColor: theme.background,
+      borderBottomWidth: 0.5,
+      borderBottomColor: theme.lowEmphasis,
+    },
+    tab: {
+      flex: 1,
+      alignItems: "center",
+      paddingVertical: 8,
+    },
+    selectedTab: {
+      borderBottomWidth: 3,
+      borderBottomColor: theme.primary,
+    },
+    tabText: {
+      color: theme.lowEmphasis,
+      fontSize: 18,
+      fontWeight: "bold",
+    },
+    selectedTabText: {
+      color: theme.primary,
+    },
+  });
--- sweep/src/pages/home/index.tsx
+++ sweep/src/pages/home/index.tsx
@@ -1,5 +1,11 @@
 import { HomeLayout } from "./_layouts/HomeLayout";
-import { AcademyDetail } from "./AcademyDetail/AcademyDetail";
+import { AcademyDetailLayout } from "./_layouts/AcademyDetailLayout";
+
+import { AcademyInformation } from "./AcademyDetails/Information/AcademyInformation";
+import { AcademyPrograms } from "./AcademyDetails/Programs/AcademyPrograms";
+import { AcademyCoaches } from "./AcademyDetails/Coaches/AcademyCoaches";
+import { AcademyNotices } from "./AcademyDetails/Notices/AcademyNotices";
+
 import { CoachDetail } from "./AcademyDetail/CoachDetail/CoachDetail";
 import { Home } from "./Home";
 import { MyAcademy } from "./MyAcademy/MyAcademy";
@@ -8,7 +14,11 @@ import { Reservation } from "./Reservation/Reservation";
 
 export {
   HomeLayout,
-  AcademyDetail,
+  AcademyDetailLayout,
+  AcademyInformation,
+  AcademyPrograms,
+  AcademyCoaches,
+  AcademyNotices,
   CoachDetail,
   MyAcademy,
   Home,
