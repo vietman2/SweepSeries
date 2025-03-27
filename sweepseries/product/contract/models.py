e4ef31c32d5420479324d6697699a38a8ad46394
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-27 17:26:49 +0900

    add new models and migrations

--- sweepseries/product/contract/models.py
+++ sweepseries/product/contract/models.py
@@ -20,3 +20,71 @@ class Contract(TimeStampedModel):
     class Meta:
         db_table = 'contract'
         ordering = ['-created_at']
+
+class Tag(models.Model):
+    tag         = models.CharField(max_length=50)
+    is_positive = models.BooleanField(default=True)
+
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+class LessonReviewTags(Tag):
+    class Meta:
+        db_table = 'lesson_review_tags'
+
+class CoachReviewTags(Tag):
+    class Meta:
+        db_table = 'coach_review_tags'
+
+class AcademyReviewTags(Tag):
+    class Meta:
+        db_table = 'academy_review_tags'
+
+class ReviewImage(TimeStampedModel):
+    image   = models.ImageField(upload_to="reviews/")
+
+    objects = models.Manager()
+
+    class Meta:
+        db_table = 'review_image'
+
+class Review(TimeStampedModel):
+    contract        = models.OneToOneField(
+        Contract, on_delete=models.CASCADE, related_name='review'
+    )
+    lesson_rating   = models.PositiveSmallIntegerField()
+    lesson_comment  = models.CharField(max_length=500)
+    lesson_tags     = models.ManyToManyField(
+        LessonReviewTags, related_name='lesson_reviews'
+    )
+    lesson_images   = models.ManyToManyField(
+        ReviewImage, related_name='lesson_reviews'
+    )
+
+    coach_rating    = models.PositiveSmallIntegerField()
+    coach_comment   = models.CharField(max_length=500)
+    secure_coach    = models.BooleanField(default=False)
+    coach_tags      = models.ManyToManyField(
+        CoachReviewTags, related_name='coach_reviews'
+    )
+    coach_images    = models.ManyToManyField(
+        ReviewImage, related_name='coach_reviews'
+    )
+
+    academy_rating  = models.PositiveSmallIntegerField()
+    academy_comment = models.CharField(max_length=500)
+    secure_academy  = models.BooleanField(default=False)
+    academy_tags    = models.ManyToManyField(
+        AcademyReviewTags, related_name='academy_reviews'
+    )
+    academy_images  = models.ManyToManyField(
+        ReviewImage, related_name='academy_reviews'
+    )
+
+    objects         = models.Manager()
+
+    class Meta:
+        db_table = 'review'
+        ordering = ['-created_at']
