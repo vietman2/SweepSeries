7ab49413755622efbe9cbece9fdbb8c21e89ae7b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-28 21:00:47 +0900

    route new fragments

--- sweep/src/fragments/Review/index.tsx
+++ sweep/src/fragments/Review/index.tsx
@@ -1,6 +1,13 @@
 import { ReviewInputs } from "./ReviewInputs/ReviewInputs";
 import { ReviewsHeader } from "./ReviewsHeader/ReviewsHeader";
 import { ReviewSimple } from "./ReviewSimple/ReviewSimple";
+import { ReviewsSummary } from "./ReviewsSummary/ReviewsSummary";
 import { SessionsToReview } from "./SessionsToReview/SessionsToReview";
 
-export { ReviewInputs, ReviewsHeader, ReviewSimple, SessionsToReview };
+export {
+  ReviewInputs,
+  ReviewsHeader,
+  ReviewSimple,
+  ReviewsSummary,
+  SessionsToReview,
+};
