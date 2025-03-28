6ef437136974155d9c3af4b11b9ec7dced63d63a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-28 20:48:47 +0900

    integrate api

--- sweep/src/pages/home/AcademyDetails/Reviews/AcademyReviews.test.tsx
+++ sweep/src/pages/home/AcademyDetails/Reviews/AcademyReviews.test.tsx
@@ -1,13 +1,62 @@
+import { fireEvent, waitFor } from "@testing-library/react-native";
+
 import { AcademyReviews } from "./AcademyReviews";
+import * as AcademyContext from "@contexts/academy";
+import * as ReviewsAPI from "@services/products/reviews";
+import {
+  sampleAcademyDetail,
+  sampleReviewResponse,
+  sampleReviews,
+  sampleReviewSummary,
+} from "@testdata/products";
 import { renderWithProviders } from "@utils/test-utils";
 
 jest.mock("@fragments/Review", () => ({
-  ReviewsHeader: () => "ReviewsHeader",
+  ReviewsSummary: () => "ReviewsSummary",
   ReviewSimple: () => "ReviewSimple",
 }));
 
 describe("<AcademyReviews />", () => {
-  it("renders correctly", () => {
+  const defaultContext = {
+    academy: sampleAcademyDetail,
+    showDetailPage: false,
+    selectCoach: jest.fn(),
+    selectNotice: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest
+      .spyOn(AcademyContext, "useAcademyDetail")
+      .mockReturnValue(defaultContext);
+    jest
+      .spyOn(ReviewsAPI, "getAcademyReviewSummary")
+      .mockResolvedValue(sampleReviewSummary);
+    jest
+      .spyOn(ReviewsAPI, "getAcademyReviews")
+      .mockResolvedValue({ ...sampleReviewResponse, results: sampleReviews });
+  });
+
+  it("renders and handles load more correctly", async () => {
+    const { getByTestId } = renderWithProviders(<AcademyReviews />);
+
+    await waitFor(() => {
+      fireEvent.press(getByTestId("load-more"));
+    });
+  });
+
+  it("handles bad api response", async () => {
+    jest.spyOn(ReviewsAPI, "getAcademyReviews").mockResolvedValue(null);
+
+    waitFor(() => {
+      renderWithProviders(<AcademyReviews />);
+    });
+  });
+
+  it("handles bad config", async () => {
+    jest
+      .spyOn(AcademyContext, "useAcademyDetail")
+      .mockReturnValue({ ...defaultContext, academy: null });
+
     renderWithProviders(<AcademyReviews />);
   });
 });
--- sweep/src/pages/home/AcademyDetails/Reviews/AcademyReviews.tsx
+++ sweep/src/pages/home/AcademyDetails/Reviews/AcademyReviews.tsx
@@ -1,26 +1,54 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import { Divider } from "@components/Dividers";
+import { AppIcon } from "@components/Icons";
+import { useAcademyDetail } from "@contexts/academy";
 import { useTheme } from "@contexts/theme";
-import { ReviewsHeader, ReviewSimple } from "@fragments/Review";
-import { ReviewType } from "@models/products";
-import { sampleReviews } from "@testdata/products";
+import { ReviewsSummary, ReviewSimple } from "@fragments/Review";
+import {
+  AcademyReviewSummaryType,
+  ReviewResponseType,
+  AcademyReviewType,
+} from "@models/products";
+import { getAcademyReviews, getAcademyReviewSummary } from "@services/products";
 import { ThemeColorType } from "@themes/colors";
 
 export function AcademyReviews() {
-  const [reviews, setReviews] = useState<ReviewType[]>([]);
+  const [result, setResult] = useState<ReviewResponseType>();
+  const [reviews, setReviews] = useState<AcademyReviewType[]>([]);
+  const [summary, setSummary] = useState<AcademyReviewSummaryType>();
 
+  const { academy } = useAcademyDetail();
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
   useEffect(() => {
-    setReviews(sampleReviews);
+    const fetchData = async () => {
+      if (!academy) return;
+
+      const response1 = await getAcademyReviewSummary(academy.uuid);
+      const response2 = await getAcademyReviews(academy.uuid);
+
+      if (response1 && response2) {
+        setSummary(response1);
+        setReviews(response2.results);
+        setResult(response2);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  if (!academy || !summary || !result) return null;
+
+  const handleMore = async () => {
+    // TODO: Implement pagination
+  };
 
   return (
     <View style={styles.container}>
-      <ReviewsHeader rating={4.5} />
+      <ReviewsSummary summary={summary} />
       <Divider />
       {/*TODO: Filter and Sort */}
       {reviews.map((review) => (
@@ -29,6 +57,12 @@ export function AcademyReviews() {
           <Divider />
         </View>
       ))}
+      {result.next && (
+        <TouchableOpacity onPress={handleMore} style={styles.moreButton} testID="load-more">
+          <AppIcon icon="plus" size={16} color={theme.primary} />
+          <Text style={styles.buttonText}>더보기</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -40,5 +74,17 @@ const createStyles = (theme: ThemeColorType) =>
       padding: 16,
       gap: 16,
       backgroundColor: theme.background,
+    },
+    moreButton: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 8,
+      backgroundColor: theme.background,
+    },
+    buttonText: {
+      color: theme.primary,
+      fontSize: 14,
+      marginLeft: 8,
     },
   });
