afab94f801c5b3d85c2e7032e977d5d28a2df6cc
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-01 03:53:02 +0900

    apply refactor

--- sweepseries/calendars/tests.py
+++ sweepseries/calendars/tests.py
@@ -9,7 +9,7 @@ class CalendarsAPITestCase(APITestCase):
         "core/data/test/academies.json", "core/data/initial/regions.json",
         "core/data/test/coaches.json", "core/data/initial/professions.json",
         "core/data/test/programs.json", "core/data/test/contracts.json",
-        "core/data/initial/reviewtags.json",
+        "core/data/initial/reviewtags.json", "core/data/test/lessons.json",
     ]
 
     def setUp(self):
