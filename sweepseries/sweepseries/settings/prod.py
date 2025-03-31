270e9c56600e2da4d3303f209e25a3b59fe930e9
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-01 02:48:36 +0900

    Add config

--- sweepseries/sweepseries/settings/prod.py
+++ sweepseries/sweepseries/settings/prod.py
@@ -77,6 +77,7 @@ AWS_S3_ENDPOINT_URL = 'https://kr.object.ncloudstorage.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = 'https://kr.object.ncloudstorage.com/sweepseries/'
