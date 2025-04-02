c88e0dea495322bd8fd90409d4bbe9db6b231476
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-02 11:36:19 +0900

    refactor serializers

---
+++ sweepseries/product/lesson/serializers/__init__.py
@@ -0,0 +1,10 @@
+from .lesson_serializer import LessonSerializer
+from .session_serializers import SessionSerializer, SessionDetailSerializer
+from .session_request_serializer import SessionRequestSerializer
+
+__all__ = [
+    'LessonSerializer',
+    'SessionSerializer',
+    'SessionDetailSerializer',
+    'SessionRequestSerializer'
+]
