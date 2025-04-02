507a78af8e2be84ff8f03b99e3989963fc72a396
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-02 16:38:45 +0900

    route new apis

--- sweep/src/services/calendar/sessions.ts
+++ sweep/src/services/calendar/sessions.ts
@@ -22,6 +22,24 @@ export async function getSessionDetails(sessionId: string) {
   }
 }
 
+export async function getSessionAvailableTimes(
+  sessionId: string,
+  date: string
+) {
+  try {
+    const response = await axios.get(
+      `/v1/sessions/${sessionId}/available_times/`,
+      {
+        params: { date },
+      }
+    );
+
+    return response.data;
+  } catch {
+    return null;
+  }
+}
+
 export async function updateSessionNotes(sessionId: string, notes: string) {
   try {
     await axios.patch(`/v1/sessions/${sessionId}/`, { notes });
@@ -40,6 +58,23 @@ export async function updateSessionFeedback(
     await axios.patch(`/v1/sessions/${sessionId}/`, { feedback });
 
     return true;
+  } catch {
+    return null;
+  }
+}
+
+export async function requestSessionScheduleChange(
+  sessionId: string,
+  date: string,
+  time: string
+) {
+  try {
+    const response = await axios.post(
+      `/v1/sessions/${sessionId}/schedule_change/`,
+      { date, time }
+    );
+
+    return response.data;
   } catch {
     return null;
   }
