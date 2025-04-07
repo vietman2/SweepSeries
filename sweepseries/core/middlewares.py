5300a2f6f50d098928deeb057d89dd528b0c721b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-07 10:09:11 +0900

    fix issue

--- sweepseries/core/middlewares.py
+++ sweepseries/core/middlewares.py
@@ -33,6 +33,8 @@ class BadResponseMiddleware(MiddlewareMixin):
 
         logger.error(log_message)
 
+        return None
+
     def process_response(self, request, response):
         if settings.DEBUG:
             return response
