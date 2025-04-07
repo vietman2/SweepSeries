506d24204041c70e7bc38dbdeff5d54641b0b2ba
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-07 10:00:46 +0900

    fix middleware

--- sweepseries/core/middlewares.py
+++ sweepseries/core/middlewares.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 
 logger = logging.getLogger('gunicorn.error')
@@ -16,25 +17,15 @@ class DisableCookiesMiddleware(MiddlewareMixin):
 
 class BadResponseMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
+        if settings.DEBUG:
+            return None
+
         tb = traceback.format_exc()
-
-        try:
-            body = request.body.decode(errors='ignore')
-        except Exception:
-            body = '<unavailable>'
-
-        try:
-            post_data = request.POST.dict()
-        except Exception:
-            post_data = '<unavailable>'
 
         log_message = (
             "Unhandled Exception:\n"
             f"Path: {request.path}\n"
             f"Method: {request.method}\n"
-            f"GET Params: {request.GET.dict()}\n"
-            f"POST Data: {post_data}\n"
-            f"Body: {body}\n"
             f"User: {getattr(request, 'user', None)}\n"
             f"Exception: {str(exception)}\n"
             f"Traceback:\n{tb}"
@@ -43,17 +34,17 @@ class BadResponseMiddleware(MiddlewareMixin):
         logger.error(log_message)
 
     def process_response(self, request, response):
+        if settings.DEBUG:
+            return response
+
         if response.status_code >= 400:
             log_message = (
                 f"Client/Server Error Response:\n"
                 f"Status Code: {response.status_code}\n"
                 f"Path: {request.path}\n"
                 f"Method: {request.method}\n"
-                f"GET: {request.GET.dict()}\n"
-                f"POST: {getattr(request, 'POST', {})}\n"
-                f"Body: {request.body.decode(errors='ignore')}\n"
                 f"User: {getattr(request, 'user', None)}\n"
-                f"Response Content: {getattr(response, 'content', b'').decode(errors='ignore')[:500]}"
+                f"Response: {getattr(response, 'content', b'').decode(errors='ignore')[:500]}"
             )
 
             logger.error(log_message)
