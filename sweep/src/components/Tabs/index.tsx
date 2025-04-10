4d854533a000395a38d9bc813f1d5bf3b79a21e7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-10 18:03:37 +0900

    remove custom collapsible tabs

--- sweep/src/components/Tabs/index.tsx
+++ sweep/src/components/Tabs/index.tsx
@@ -1,5 +1,4 @@
-import { CollapsibleTab } from "./CollapsibleTab";
 import { FAQTabs } from "./FAQTabs";
 import { TabBar } from "./Tabbar";
 
-export { CollapsibleTab, FAQTabs, TabBar };
+export { FAQTabs, TabBar };
