e3228fef8e9f48eadcd8caf6f3d339cf192dbf02
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-11 11:26:22 +0900

    update frontlayout

--- sweep/src/pages/front/_layouts/FrontLayout.test.tsx
+++ sweep/src/pages/front/_layouts/FrontLayout.test.tsx
@@ -1,39 +1,85 @@
 import { fireEvent } from "@testing-library/react-native";
+import * as Router from "expo-router";
 
 import { FrontLayout } from "./FrontLayout";
-import * as FrontContext from "@contexts/front";
-import { sampleAcademies, sampleCoaches } from "@testdata/products";
+import * as FrontContexts from "@contexts/front";
 import { renderWithProviders } from "@utils/test-utils";
+import { sampleAcademyProfiles, sampleCoachProfiles } from "@testdata/products";
+
+jest.mock("@fragments/Profile", () => ({
+  SelectedProfile: () => <></>,
+  PromodeProfile: () => <></>,
+}));
 
 describe("<FrontLayout />", () => {
+  const defaultProfile = {
+    uuid: sampleAcademyProfiles[0].uuid,
+    name: sampleAcademyProfiles[0].name,
+    image: sampleAcademyProfiles[0].logo,
+  };
   const defaultContext = {
-    uuid: "1",
-    academies: sampleAcademies,
-    coach: sampleCoaches[0],
-    headerImage: "",
-    headerText: "",
+    academies: sampleAcademyProfiles,
+    coaches: sampleCoachProfiles,
+    isReady: true,
+    refreshProfile: jest.fn(),
     selectAcademy: jest.fn(),
     selectCoach: jest.fn(),
-    refresh: jest.fn(),
   };
 
-  it("renders correctly and handles back press", () => {
-    jest
-      .spyOn(FrontContext, "useFront")
-      .mockReturnValue({ ...defaultContext, mode: "academy", uuid: "2" });
-    const { getAllByTestId } = renderWithProviders(<FrontLayout />);
-
-    fireEvent.press(getAllByTestId("back-button")[0]);
+  beforeEach(() => {
+    jest.spyOn(Router, "usePathname").mockReturnValue("/front/academy");
+    jest.spyOn(FrontContexts, "useFront").mockReturnValue({
+      ...defaultContext,
+      activeProfile: { ...defaultProfile, mode: "academy" },
+    });
   });
 
-  it("renders correctly and handles bottom sheet", () => {
-    jest
-      .spyOn(FrontContext, "useFront")
-      .mockReturnValue({ ...defaultContext, mode: "coach" });
+  it("renders tabs correctly and handles profile change", () => {
     const { getByTestId } = renderWithProviders(<FrontLayout />);
 
     fireEvent.press(getByTestId("opensheet"));
-    fireEvent.press(getByTestId("academy-1"));
-    fireEvent.press(getByTestId("coach"));
+    fireEvent.press(getByTestId("academy-2"));
+    fireEvent.press(getByTestId("coach-1"));
+  });
+
+  it("renders lesson details page", () => {
+    jest.spyOn(Router, "usePathname").mockReturnValue("/front/lesson/1");
+    jest.spyOn(FrontContexts, "useFront").mockReturnValue({
+      ...defaultContext,
+      activeProfile: { ...defaultProfile, mode: "academy" },
+    });
+
+    const { getByTestId } = renderWithProviders(<FrontLayout />);
+
+    fireEvent.press(getByTestId("back-button"));
+  });
+
+  it("renders error page", () => {
+    jest.spyOn(FrontContexts, "useFront").mockReturnValue({
+      ...defaultContext,
+      academies: [],
+      coaches: [],
+      activeProfile: {
+        uuid: sampleAcademyProfiles[0].uuid,
+        name: sampleAcademyProfiles[0].name,
+        image: sampleAcademyProfiles[0].logo,
+        mode: "academy",
+      },
+    });
+
+    renderWithProviders(<FrontLayout />);
+  });
+
+  it("renders loading", () => {
+    jest.spyOn(FrontContexts, "useFront").mockReturnValue({
+      ...defaultContext,
+      activeProfile: {
+        ...defaultProfile,
+        mode: "academy",
+      },
+      isReady: false,
+    });
+
+    renderWithProviders(<FrontLayout />);
   });
 });
