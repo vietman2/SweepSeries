8d34116633db7569f70028a239d1bbd30f6f2b41
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-11 11:26:33 +0900

    remove old pages

--- sweep/src/pages/front/index.tsx
+++ sweep/src/pages/front/index.tsx
@@ -1,17 +1,41 @@
 import { FrontLayout } from "./_layouts/FrontLayout";
-import { FrontTopTabs } from "./FrontTopTabs";
 
-import { CreateProgram } from "./_modals/CreateProgram/CreateProgram";
+import { AcademyProfileManagement } from "./ProfileManagement/AcademyProfile/AcademyProfileManagement";
+import { CoachProfileManagement } from "./ProfileManagement/CoachProfile/CoachProfileManagement";
+import { ProgramManagement } from "./ProgramManagement/ProgramManagement";
+import { CreateProgram } from "./ProgramManagement/CreateProgram";
+import { EmployeeManagement } from "./EmployeeManagement/EmployeeManagement";
+import { NoticeManagement } from "./NoticeManagement/NoticeManagement";
+import {
+  AcademyReviewManagement,
+  CoachReviewManagement,
+} from "./ReviewManagement/ReviewManagement";
+import {
+  AcademyCustomerManagement,
+  CoachCustomerManagement,
+} from "./CustomerManagement/CustomerManagement";
 
-import { CustomerDetail } from "./CustomerManagement/CustomerDetail";
+import {
+  AcademyCustomerDetail,
+  CoachCustomerDetail,
+} from "./CustomerManagement/CustomerDetail";
 import { NoticeEdit } from "./NoticeManagement/NoticeEdit";
 import { ProgramEdit } from "./ProgramManagement/ProgramEdit";
 
 export {
   FrontLayout,
+  AcademyProfileManagement,
+  CoachProfileManagement,
+  ProgramManagement,
+  NoticeManagement,
+  AcademyReviewManagement,
+  CoachReviewManagement,
+  AcademyCustomerManagement,
+  CoachCustomerManagement,
   CreateProgram,
-  CustomerDetail,
-  FrontTopTabs,
+  EmployeeManagement,
+  AcademyCustomerDetail,
+  CoachCustomerDetail,
   NoticeEdit,
   ProgramEdit,
 };
