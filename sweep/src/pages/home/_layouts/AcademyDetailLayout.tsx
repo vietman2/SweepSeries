740b6710d9325aebc292365f57741fa9c5bc7467
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-14 16:13:56 +0900

    update layout

--- sweep/src/pages/home/_layouts/AcademyDetailLayout.tsx
+++ sweep/src/pages/home/_layouts/AcademyDetailLayout.tsx
@@ -55,7 +55,7 @@ const tabs: AcademyDetailTabType[] = [
 ];
 
 function AcademyDetail() {
-  const { academy, showDetailPage, loading, refresh } = useAcademyDetail();
+  const { academy, loading, refresh } = useAcademyDetail();
   const pathname = usePathname();
   const { theme } = useTheme();
   const styles = createStyles(theme);
@@ -66,10 +66,6 @@ function AcademyDetail() {
 
   if (!academy) {
     return null;
-  }
-
-  if (showDetailPage) {
-    return <Slot />;
   }
 
   const handleTabPress = (tab: AcademyDetailTabType) => {
