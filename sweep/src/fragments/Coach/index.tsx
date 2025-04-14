4ebe6a5cf21cc754e69231588697e634bbbf0ff9
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-14 15:49:45 +0900

    route

--- sweep/src/fragments/Coach/index.tsx
+++ sweep/src/fragments/Coach/index.tsx
@@ -1,16 +1,25 @@
 import { CoachModal } from "./CoachModal/CoachModal";
 import {
+  CoachIntroduction,
+  CoachIntroductionEdit,
+} from "./CoachProfile/CoachIntroduction";
+import {
   CoachProfileImage,
   CoachProfileImageEdit,
 } from "./CoachProfile/CoachProfileImage";
+import { CoachSNS, CoachSNSEdit } from "./CoachProfile/CoachSNS";
 import { CoachRequest } from "./CoachRequest/CoachRequest";
 import { CoachSelect } from "./CoachSelect/CoachSelect";
 import { CoachSimple } from "./CoachSimple/CoachSimple";
 
 export {
   CoachModal,
+  CoachIntroduction,
+  CoachIntroductionEdit,
   CoachProfileImage,
   CoachProfileImageEdit,
+  CoachSNS,
+  CoachSNSEdit,
   CoachRequest,
   CoachSelect,
   CoachSimple,
